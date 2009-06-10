9fcac1639c8cad5188b74bb0daed52f3952925ec
Christian Grothoff <christian@grothoff.org>
2009-06-10 15:06:09 +0000

    script

--- contrib/regression.sh
+++ contrib/regression.sh
@@ -1,18 +1,54 @@
 #!/bin/sh
 cd
 cd gnunet
-svn up
-echo "================START===================" >> regression.txt
-uname -a >> regression.txt
-date >> regression.txt
-svn up >> regression.txt
+svn up > /dev/null
+H=`hostname`
+echo "================START===================" >> regression.$H
+RUN=`date +%0y%0m%0d%0k%0M`
+uname -a >> regression.$H
+date >> regression.$H
+echo "Run $RUN" >> regression.$H
+svn up >> regression.$H
 export GNUNET_PREFIX=$HOME
 export PATH=$HOME/bin:$PATH
-./bootstrap
-./configure --prefix=$HOME --with-extractor=$HOME --with-microhttpd=$HOME
-make clean
-make install
-make check || echo Failed >> regression.txt
-make check && echo Success >> regression.txt
-echo "================END====================" >> regression.txt
-svn commit -m reg regression.txt
+./bootstrap >> regression.$H.$RUN  2>&1
+./configure --prefix=$HOME --with-extractor=$HOME --with-microhttpd=$HOME  >> regression.$H.$RUN  2>&1
+if [ $? != 0 ]
+then
+  echo configure failed >> regression.$H
+  exit
+fi
+KEEP=0
+make clean >> regression.$H.$RUN 2>&1
+make install >> regression.$H.$RUN 2>&1
+cd src
+for n in `ls --hide "Makefile*"`
+do
+  cd $n
+  if [ -f Makefile ]
+  then
+    make check >> ../regression.$H.$RUN.$n  2>&1
+    if [ $? != 0 ]
+    then
+      echo Tests for $n failed >> ../regression.$H
+      echo "--------- Details for $n -----------" >> ../regression.$H
+      cat regression.$H.$RUN.$n >> ../regression.$H
+      echo "----- End of Details for $n --------" >> ../regression.$H
+      KEEP=1
+    else
+      echo Tests for $n succeeded >> ../regression.$H
+    fi
+  fi
+  cd ..
+done
+echo "================END====================" >> regression.$H
+
+if [ $KEEP == 0]
+then
+  rm regression.$H.$RUN*
+  rm regression.$H
+else
+  svn add regression.$H > /dev/null
+  svn commit -m "Regression in tests on $H" regression.$H
+fi
+
