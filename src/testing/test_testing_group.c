d5128d1f3cbaf2e495ae1325b5c133cafca9437e
Christian Grothoff <christian@grothoff.org>
2009-10-31 21:57:40 +0000

    group test works

--- src/testing/test_testing_group.c
+++ src/testing/test_testing_group.c
@@ -18,7 +18,7 @@
      Boston, MA 02111-1307, USA.
 */
 /**
- * @file testing/test_testing_connect.c
+ * @file testing/test_testing_group.c
  * @brief testcase for functions to connect two peers in testing.c
  */
 #include "platform.h"
@@ -35,83 +35,26 @@
 
 static int ok;
 
-static struct GNUNET_TESTING_Daemon *d1;
+static int peers_left;
 
-static struct GNUNET_TESTING_Daemon *d2;
-
-static struct GNUNET_CONFIGURATION_Handle *c1;
-
-static struct GNUNET_CONFIGURATION_Handle *c2;
+static struct GNUNET_TESTING_PeerGroup *pg;
 
 static struct GNUNET_SCHEDULER_Handle *sched;
 
-static void end2_cb(void *cls,
-		   const char *emsg)
-{
-  GNUNET_assert (emsg == NULL); 
-#if VERBOSE
-  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
-	      "Daemon terminated, will now exit.\n");
-#endif
- ok = 0;
-}
 
-static void end1_cb(void *cls,
-		    const char *emsg)
-{
-  GNUNET_assert (emsg == NULL); 
-  GNUNET_TESTING_daemon_stop (d2, &end2_cb, NULL);
-  d2 = NULL;
-}
-
-
-static void
-my_connect_complete (void *cls,
-		     const char *emsg)
-{
-  GNUNET_TESTING_daemon_stop (d1, &end1_cb, NULL);
-  d1 = NULL;
-}
-
-
-static void my_cb2(void *cls,
+static void my_cb(void *cls,
 		   const struct GNUNET_PeerIdentity *id,
 		   const struct GNUNET_CONFIGURATION_Handle *cfg,
 		   struct GNUNET_TESTING_Daemon *d,
 		   const char *emsg)
 {
   GNUNET_assert (id != NULL);
-#if VERBOSE
-  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
-	      "Daemon `%s' started.\n",
-	      GNUNET_i2s (id));
-#endif
-  GNUNET_TESTING_daemons_connect (d1, d2,
-				  TIMEOUT,
-				  &my_connect_complete,
-				  NULL);
-}
-
-
-static void my_cb1(void *cls,
-		   const struct GNUNET_PeerIdentity *id,
-		   const struct GNUNET_CONFIGURATION_Handle *cfg,
-		   struct GNUNET_TESTING_Daemon *d,
-		   const char *emsg)
-{
-  GNUNET_assert (id != NULL);
-#if VERBOSE
-  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
-	      "Daemon `%s' started.\n",
-	      GNUNET_i2s (id));
-#endif
-  d2 = GNUNET_TESTING_daemon_start (sched,
-				    c2,
-				    NULL,
-				    &my_cb2,
-				    NULL);
-  GNUNET_assert (d2 != NULL);
-
+  peers_left--;
+  if (peers_left == 0)
+    {
+      GNUNET_TESTING_daemons_stop (pg);
+      ok = 0;
+    }
 }
 
 
@@ -126,20 +69,13 @@ run (void *cls,
   ok = 1;
 #if VERBOSE
   GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
-	      "Starting daemon.\n");
+	      "Starting daemons.\n");
 #endif
-  c1 = GNUNET_CONFIGURATION_create ();
-  GNUNET_CONFIGURATION_parse (c1,
-			      "test_testing_connect_peer1.conf");
-  c2 = GNUNET_CONFIGURATION_create ();
-  GNUNET_CONFIGURATION_parse (c2,
-			      "test_testing_connect_peer2.conf");
-  d1 = GNUNET_TESTING_daemon_start (sched,
-				    c1,
-				    NULL,
-				    &my_cb1,
-				    NULL);
-  GNUNET_assert (d1 != NULL);
+  peers_left = 4;
+  pg = GNUNET_TESTING_daemons_start (sched, cfg, 
+				     peers_left,
+				     &my_cb, NULL, NULL);
+  GNUNET_assert (pg != NULL);
 }
 
 static int
@@ -157,7 +93,7 @@ check ()
     GNUNET_GETOPT_OPTION_END
   };
   GNUNET_PROGRAM_run ((sizeof (argv) / sizeof (char *)) - 1,
-                      argv, "test-testing-connect", "nohelp",
+                      argv, "test-testing-group", "nohelp",
                       options, &run, &ok);
   return ok;
 }
@@ -167,7 +103,7 @@ main (int argc, char *argv[])
 {
   int ret;
 
-  GNUNET_log_setup ("test-testing-connect",
+  GNUNET_log_setup ("test-testing-group",
 #if VERBOSE
                     "DEBUG",
 #else
@@ -177,9 +113,7 @@ main (int argc, char *argv[])
   ret = check ();
   sleep (1);
   GNUNET_DISK_directory_remove ("/tmp/test-gnunet-testing");
-  GNUNET_DISK_directory_remove ("/tmp/test-gnunet-testing-connect-peer1");
-  GNUNET_DISK_directory_remove ("/tmp/test-gnunet-testing-connect-peer2");
   return ret;
 }
 
-/* end of test_testing_connect.c */
+/* end of test_testing_group.c */
