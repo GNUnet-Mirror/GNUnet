0fc5338f8db1662d8ae11ef9b2ccfcdecd3a3eb2
Christian Grothoff <christian@grothoff.org>
2009-11-16 14:03:01 +0000

    fix

--- src/util/program_lib_argz.c
+++ src/util/program_lib_argz.c
@@ -16,9 +16,6 @@
    with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. */
 
-#include <config.h>
-
-#include <argz.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
