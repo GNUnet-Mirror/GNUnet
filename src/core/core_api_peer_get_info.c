7b37da61d3347e75c19c30876220a32923e75dd4
Christian Grothoff <christian@grothoff.org>
2010-01-21 14:25:51 +0000

    fixing code to match new API

--- src/core/core_api_peer_get_info.c
+++ src/core/core_api_peer_get_info.c
@@ -66,7 +66,7 @@ receive_info (void *cls,
     {
       if (irc->info != NULL)
 	irc->info (irc->info_cls, 
-		   NULL, 0, 0, GNUNET_TIME_UNIT_FOREVER_REL, 0, 0);     
+		   NULL, 0, 0, 0, 0);     
       GNUNET_CLIENT_disconnect (irc->client);
       GNUNET_free (irc);
       return;
