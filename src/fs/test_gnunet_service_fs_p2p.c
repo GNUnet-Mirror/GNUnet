1c5a459f771747138189e06fa5f261f48365a4d5
Christian Grothoff <christian@grothoff.org>
2010-03-08 20:44:13 +0000

    relax

--- src/fs/test_gnunet_service_fs_p2p.c
+++ src/fs/test_gnunet_service_fs_p2p.c
@@ -36,7 +36,7 @@
 /**
  * How long until we give up on transmitting the message?
  */
-#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 60)
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 120)
 
 #define NUM_DAEMONS 2
 
