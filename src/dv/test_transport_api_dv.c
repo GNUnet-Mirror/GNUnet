8f7beb8f68db072d4ebf6c3633a21b46513791c9
Nathan S. Evans <evans@in.tum.de>
2010-03-11 13:45:58 +0000

    incremental improvements, testcase which (should) work once dv works

---
+++ src/dv/test_transport_api_dv.c
@@ -0,0 +1,353 @@
+/*
+     This file is part of GNUnet.
+     (C) 2009 Christian Grothoff (and other contributing authors)
+
+     GNUnet is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 2, or (at your
+     option) any later version.
+
+     GNUnet is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with GNUnet; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+/**
+ * @file transport/test_transport_api_dv.c
+ * @brief base test case for dv transport (separated from other transport
+ * testcases for two reasons. 1) dv-service relies on core and other
+ * transport plugins, dv plugin relies on dv-service, so dv-plugin needs
+ * to live here, and 2) a dv plugin testcase is different from other
+ * tranport plugin testcases because we need at least three peer to test
+ * it.
+ *
+ * This test case tests DV functionality.  Specifically it starts three
+ * peers connected in a line (1 <-> 2 <-> 3).  Then a message is transmitted
+ * from peer 1 to peer 3.  Assuming that DV is working, peer 2 should have
+ * gossiped about peer 3 to 1, and should then forward a message from one
+ * to 3.
+ */
+#include "platform.h"
+#include "gnunet_common.h"
+#include "gnunet_hello_lib.h"
+#include "gnunet_getopt_lib.h"
+#include "gnunet_os_lib.h"
+#include "gnunet_program_lib.h"
+#include "gnunet_scheduler_lib.h"
+#include "gnunet_transport_service.h"
+#include "../transport/transport.h"
+
+#define VERBOSE GNUNET_YES
+
+#define VERBOSE_ARM GNUNET_YES
+
+#define START_ARM GNUNET_YES
+
+/**
+ * How long until we give up on transmitting the message?
+ */
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 50)
+
+#define MTYPE 12345
+
+struct PeerContext
+{
+  struct GNUNET_CONFIGURATION_Handle *cfg;
+  struct GNUNET_TRANSPORT_Handle *th;
+  struct GNUNET_PeerIdentity id;
+#if START_ARM
+  pid_t arm_pid;
+#endif
+};
+
+static struct PeerContext p1;
+
+static struct PeerContext p2;
+
+static struct PeerContext p3;
+
+static struct GNUNET_SCHEDULER_Handle *sched;
+
+static int ok;
+
+GNUNET_SCHEDULER_TaskIdentifier die_task;
+
+#if VERBOSE
+#define OKPP do { ok++; fprintf (stderr, "Now at stage %u at %s:%u\n", ok, __FILE__, __LINE__); } while (0)
+#else
+#define OKPP do { ok++; } while (0)
+#endif
+
+
+static void
+end ()
+{
+  /* do work here */
+  GNUNET_assert (ok == 6);
+  GNUNET_SCHEDULER_cancel (sched, die_task);
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "Disconnecting from transports!\n");
+  GNUNET_TRANSPORT_disconnect (p1.th);
+  GNUNET_TRANSPORT_disconnect (p2.th);
+  GNUNET_TRANSPORT_disconnect (p3.th);
+
+  die_task = GNUNET_SCHEDULER_NO_TASK;
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "Transports disconnected, returning success!\n");
+  ok = 0;
+}
+
+static void
+stop_arm (struct PeerContext *p)
+{
+#if START_ARM
+  if (0 != PLIBC_KILL (p->arm_pid, SIGTERM))
+    GNUNET_log_strerror (GNUNET_ERROR_TYPE_WARNING, "kill");
+  GNUNET_OS_process_wait (p->arm_pid);
+#endif
+  GNUNET_CONFIGURATION_destroy (p->cfg);
+}
+
+
+static void
+end_badly ()
+{
+  /* do work here */
+#if VERBOSE
+  fprintf(stderr, "Ending on an unhappy note.\n");
+#endif
+
+  GNUNET_TRANSPORT_disconnect (p1.th);
+  GNUNET_TRANSPORT_disconnect (p2.th);
+  GNUNET_TRANSPORT_disconnect (p3.th);
+
+  ok = 1;
+  return;
+}
+
+static void
+notify_receive (void *cls,
+                const struct GNUNET_PeerIdentity *peer,
+                const struct GNUNET_MessageHeader *message,
+                struct GNUNET_TIME_Relative latency,
+		uint32_t distance)
+{
+
+  if (ntohs(message->type) != MTYPE)
+    return;
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "Received message of type %d from peer (%p)!\n",
+                ntohs(message->type), cls);
+
+  GNUNET_assert (ok == 5);
+  OKPP;
+
+  GNUNET_assert (MTYPE == ntohs (message->type));
+  GNUNET_assert (sizeof (struct GNUNET_MessageHeader) ==
+                 ntohs (message->size));
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "Received message from peer (%p)!\n",
+              cls);
+  end ();
+}
+
+
+static size_t
+notify_ready (void *cls, size_t size, void *buf)
+{
+  struct GNUNET_MessageHeader *hdr;
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Transmitting message to peer (%p) - %u!\n", cls, size);
+  GNUNET_assert (size >= 256);
+  GNUNET_assert (ok == 4);
+  OKPP;
+  if (buf != NULL)
+  {
+    hdr = buf;
+    hdr->size = htons (sizeof (struct GNUNET_MessageHeader));
+    hdr->type = htons (MTYPE);
+  }
+
+  return sizeof (struct GNUNET_MessageHeader);
+}
+
+
+static void
+notify_connect (void *cls,
+                const struct GNUNET_PeerIdentity *peer,
+                struct GNUNET_TIME_Relative latency,
+		uint32_t distance)
+{
+  if ((cls == &p1) && (memcmp(peer, &p3.id, sizeof(struct GNUNET_PeerIdentity)) == 0))
+    {
+      GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                 "Peer 1 notified about connection to peer 3!\n", GNUNET_i2s (peer), cls);
+                GNUNET_TRANSPORT_notify_transmit_ready (p1.th,
+					      &p3.id,
+					      256, 0, TIMEOUT, &notify_ready,
+					      &p1);
+    }
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Peer `%4s' connected to us (%p)!\n", GNUNET_i2s (peer), cls);
+}
+
+
+static void
+notify_disconnect (void *cls, const struct GNUNET_PeerIdentity *peer)
+{
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Peer `%4s' disconnected (%p)!\n",
+	      GNUNET_i2s (peer), cls);
+}
+
+
+static void
+setup_peer (struct PeerContext *p, const char *cfgname)
+{
+  p->cfg = GNUNET_CONFIGURATION_create ();
+#if START_ARM
+  p->arm_pid = GNUNET_OS_start_process (NULL, NULL, "gnunet-service-arm",
+                                        "gnunet-service-arm",
+#if VERBOSE_ARM
+                                        "-L", "DEBUG",
+#endif
+                                        "-c", cfgname, NULL);
+#endif
+  GNUNET_assert (GNUNET_OK == GNUNET_CONFIGURATION_load (p->cfg, cfgname));
+
+  p->th = GNUNET_TRANSPORT_connect (sched, p->cfg,
+                                    p,
+                                    &notify_receive,
+                                    &notify_connect, &notify_disconnect);
+  GNUNET_assert (p->th != NULL);
+}
+
+
+static void
+exchange_hello_next (void *cls,
+                const struct GNUNET_MessageHeader *message)
+{
+  struct PeerContext *me = cls;
+
+  GNUNET_TRANSPORT_get_hello_cancel (p2.th, &exchange_hello_next, me);
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Exchanging HELLO with peer (%p)!\n", cls);
+  GNUNET_assert (ok >= 3);
+  OKPP;
+  GNUNET_assert (message != NULL);
+  GNUNET_assert (GNUNET_OK ==
+                 GNUNET_HELLO_get_id ((const struct GNUNET_HELLO_Message *)
+                                      message, &me->id));
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Received HELLO size %d\n", GNUNET_HELLO_size((const struct GNUNET_HELLO_Message *)message));
+
+  GNUNET_TRANSPORT_offer_hello (p3.th, message);
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Finished exchanging HELLOs, now waiting for transmission!\n");
+
+}
+
+
+static void
+exchange_hello (void *cls,
+                const struct GNUNET_MessageHeader *message)
+{
+  struct PeerContext *me = cls;
+
+  GNUNET_TRANSPORT_get_hello_cancel (p1.th, &exchange_hello, me);
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Exchanging HELLO with peer (%p)!\n", cls);
+  GNUNET_assert (ok >= 2);
+  OKPP;
+  GNUNET_assert (message != NULL);
+  GNUNET_assert (GNUNET_OK ==
+                 GNUNET_HELLO_get_id ((const struct GNUNET_HELLO_Message *)
+                                      message, &me->id));
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "Received HELLO size %d\n", GNUNET_HELLO_size((const struct GNUNET_HELLO_Message *)message));
+
+  GNUNET_TRANSPORT_offer_hello (p2.th, message);
+  GNUNET_TRANSPORT_get_hello (p2.th, &exchange_hello_next, &p2);
+}
+
+static void
+run (void *cls,
+     struct GNUNET_SCHEDULER_Handle *s,
+     char *const *args,
+     const char *cfgfile, const struct GNUNET_CONFIGURATION_Handle *cfg)
+{
+  GNUNET_assert (ok == 1);
+  OKPP;
+  sched = s;
+
+  die_task = GNUNET_SCHEDULER_add_delayed (sched,
+      GNUNET_TIME_relative_multiply(GNUNET_TIME_UNIT_MINUTES, 1), &end_badly, NULL);
+
+  setup_peer (&p1, "test_transport_api_dv_peer1.conf");
+  setup_peer (&p2, "test_transport_api_dv_peer2.conf");
+  setup_peer (&p3, "test_transport_api_dv_peer3.conf");
+
+  GNUNET_assert(p1.th != NULL);
+  GNUNET_assert(p2.th != NULL);
+  GNUNET_assert(p3.th != NULL);
+
+  GNUNET_TRANSPORT_get_hello (p1.th, &exchange_hello, &p1);
+}
+
+static int
+check ()
+{
+
+  char *const argv[] = { "test-transport-api",
+    "-c",
+    "test_transport_api_data.conf",
+#if VERBOSE
+    "-L", "DEBUG",
+#endif
+    NULL
+  };
+
+  struct GNUNET_GETOPT_CommandLineOption options[] = {
+    GNUNET_GETOPT_OPTION_END
+  };
+
+  ok = 1;
+  GNUNET_PROGRAM_run ((sizeof (argv) / sizeof (char *)) - 1,
+                      argv, "test-transport-api", "nohelp",
+                      options, &run, &ok);
+  stop_arm (&p1);
+  stop_arm (&p2);
+  stop_arm (&p3);
+  return ok;
+}
+
+
+int
+main (int argc, char *argv[])
+{
+  int ret;
+#ifdef MINGW
+  return GNUNET_SYSERR;
+#endif
+
+  GNUNET_log_setup ("test-transport-api-dv",
+#if VERBOSE
+                    "DEBUG",
+#else
+                    "WARNING",
+#endif
+                    NULL);
+  ret = check ();
+  GNUNET_DISK_directory_remove ("/tmp/test-gnunetd-transport-peer-1");
+  GNUNET_DISK_directory_remove ("/tmp/test-gnunetd-transport-peer-2");
+  GNUNET_DISK_directory_remove ("/tmp/test-gnunetd-transport-peer-3");
+  return ret;
+}
+
+/* end of test_transport_api_dv.c */
