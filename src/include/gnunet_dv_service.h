5a7d07767a2cba662a70815c1069821ddc04f465
Nathan S. Evans <evans@in.tum.de>
2010-05-03 09:58:52 +0000

    doxygen

--- src/include/gnunet_dv_service.h
+++ src/include/gnunet_dv_service.h
@@ -52,7 +52,22 @@ extern "C"
  */
 struct GNUNET_DV_Handle;
 
-
+/**
+ * Send a message from the plugin to the DV service indicating that
+ * a message should be sent via DV to some peer.
+ *
+ * @param dv_handle the handle to the DV api
+ * @param target the final target of the message
+ * @param msgbuf the msg(s) to send
+ * @param msgbuf_size the size of msgbuf
+ * @param priority priority to pass on to core when sending the message
+ * @param timeout how long can this message be delayed (pass through to core)
+ * @param addr the address of this peer (internally known to DV)
+ * @param addrlen the length of the peer address
+ * @param cont continuation to call once the message has been sent (or failed)
+ * @param cont_cls closure for continuation
+ *
+ */
 int GNUNET_DV_send (struct GNUNET_DV_Handle *dv_handle,
                     const struct GNUNET_PeerIdentity *target,
                     const char *msgbuf,
