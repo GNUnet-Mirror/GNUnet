59921d3b90ed8bf823605517f6692c26a1c9dc44
Christian Grothoff <christian@grothoff.org>
2010-06-25 21:09:43 +0000

    fixing common off-by-one error with respect to maximum message size

--- src/transport/test_transport_api_reliability.c
+++ src/transport/test_transport_api_reliability.c
@@ -32,6 +32,7 @@
 #include "gnunet_os_lib.h"
 #include "gnunet_program_lib.h"
 #include "gnunet_scheduler_lib.h"
+#include "gnunet_server_lib.h"
 #include "gnunet_transport_service.h"
 #include "transport.h"
 
@@ -46,7 +47,7 @@
  * 'MAX_PENDING' in 'gnunet-service-transport.c', otherwise
  * messages may be dropped even for a reliable transport.
  */
-#define TOTAL_MSGS (60000 * 20)
+#define TOTAL_MSGS (60000 * 2)
 
 /**
  * How long until we give up on transmitting the message?
