0e35fa03c78b3e1f7c023ff554df7b315870b11c
Philipp Tölke <toelke@in.tum.de>
2010-07-20 09:41:26 +0000

    Changed the copyright-notice

--- src/vpn/gnunet-vpn-helper.c
+++ src/vpn/gnunet-vpn-helper.c
@@ -1,3 +1,28 @@
+/*
+     This file is part of GNUnet.
+     (C) 2010 Christian Grothoff
+
+     GNUnet is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 3, or (at your
+     option) any later version.
+
+     GNUnet is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with GNUnet; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+
+/**
+ * @file vpn/gnunet-daemon-vpn.c
+ * @brief
+ * @author Philipp Tölke
+ */
 #define _GNU_SOURCE
 #include <arpa/inet.h>
 #include <linux/if.h>
