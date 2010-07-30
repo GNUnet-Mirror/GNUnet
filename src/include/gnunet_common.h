1d605879a1c408422e38e7cf10f5a2682f8780aa
Nathan S. Evans <evans@in.tum.de>
2010-07-30 14:44:00 +0000

    h2s_full, useful for testing

--- src/include/gnunet_common.h
+++ src/include/gnunet_common.h
@@ -228,6 +228,16 @@ void GNUNET_logger_remove (GNUNET_Logger logger, void *logger_cls);
  */
 const char *GNUNET_h2s (const GNUNET_HashCode *hc);
 
+/**
+ * Convert a hash value to a string (for printing debug messages).
+ * This prints all 104 characters of a hashcode!
+ * This is one of the very few calls in the entire API that is
+ * NOT reentrant!
+ *
+ * @param hc the hash code
+ * @return string
+ */
+const char *GNUNET_h2s_full (const GNUNET_HashCode *hc);
 
 /**
  * Convert a peer identity to a string (for printing debug messages).
