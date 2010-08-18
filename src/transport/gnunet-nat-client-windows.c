52a4c016b3ec6a0407b2995f4a51c39a336a19af
Nathan S. Evans <evans@in.tum.de>
2010-08-18 19:07:51 +0000

    yes, windows still sucks

--- src/transport/gnunet-nat-client-windows.c
+++ src/transport/gnunet-nat-client-windows.c
@@ -393,13 +393,13 @@ send_icmp (const struct in_addr *my_ip,
   memset (&dst, 0, sizeof (dst));
   dst.sin_family = AF_INET;
   //dst.sin_addr = *other;
-  dst.sin_addr = *other;
+  dst.sin_addr = dummy;
 
   err = sendto(rawsock,
-         packet,
-         off, 0,
-         (struct sockaddr*)&dst,
-         sizeof(dst)); /* or sizeof 'struct sockaddr'? */
+               packet,
+               off, 0,
+               (struct sockaddr*)&dst,
+               sizeof(dst)); /* or sizeof 'struct sockaddr'? */
 
   fprintf(stderr, "Sent %d bytes (wanted %d)\n", err, off);
 
