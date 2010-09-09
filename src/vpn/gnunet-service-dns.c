c03148429e6f4d157e7f54d26875be14d9b0f88e
Philipp Tölke <toelke@in.tum.de>
2010-09-09 13:06:21 +0000

    answered the question

--- src/vpn/gnunet-service-dns.c
+++ src/vpn/gnunet-service-dns.c
@@ -32,6 +32,9 @@
 #include "gnunet-vpn-packet.h"
 #include "gnunet-vpn-pretty-print.h"
 
+#define WE_DO_THIS_TO_GET_THE_PORT_WE_BOUND_TO_IN_ORDER_TO_HIJACK_EVERYTHING_BUT_THIS_PORT 1
+#define WHY_ON_EARTH_DO_WE_DO_THIS WE_DO_THIS_TO_GET_THE_PORT_WE_BOUND_TO_IN_ORDER_TO_HIJACK_EVERYTHING_BUT_THIS_PORT
+
 struct dns_cls {
 	struct GNUNET_SCHEDULER_Handle *sched;
 
