ac4285d67cb9ac7cdab4f70f979184295000488b
Nathan S. Evans <evans@in.tum.de>
2010-09-14 08:34:05 +0000

    remove crazy debugging

--- src/transport/plugin_transport_tcp.c
+++ src/transport/plugin_transport_tcp.c
@@ -37,8 +37,8 @@
 #include "plugin_transport.h"
 #include "transport.h"
 
-#define DEBUG_TCP GNUNET_YES
-#define DEBUG_TCP_NAT GNUNET_YES
+#define DEBUG_TCP GNUNET_NO
+#define DEBUG_TCP_NAT GNUNET_NO
 
 /**
  * How long until we give up on transmitting the welcome message?
