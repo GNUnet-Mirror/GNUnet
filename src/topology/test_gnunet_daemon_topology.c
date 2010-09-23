956380b60d975e1a86784dae08555d6acdedb3b2
Nathan S. Evans <evans@in.tum.de>
2010-09-23 07:45:12 +0000

    even longer timeout, still timing out on hostkey generation (sparcbot)

--- src/topology/test_gnunet_daemon_topology.c
+++ src/topology/test_gnunet_daemon_topology.c
@@ -31,7 +31,7 @@
 /**
  * How long until we give up on connecting the peers?
  */
-#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 180)
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 360)
 
 #define CONNECT_ATTEMPTS 3
 
