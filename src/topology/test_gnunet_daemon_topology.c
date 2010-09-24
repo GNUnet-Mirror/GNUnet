ffc8efb1039f1a96c415ddeec1854aec9d09e896
Nathan S. Evans <evans@in.tum.de>
2010-09-24 08:17:07 +0000

    insane timeout for testcase

--- src/topology/test_gnunet_daemon_topology.c
+++ src/topology/test_gnunet_daemon_topology.c
@@ -31,7 +31,7 @@
 /**
  * How long until we give up on connecting the peers?
  */
-#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 360)
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 600)
 
 #define CONNECT_ATTEMPTS 3
 
