72f3388f4a40a48d421a5e3b568f2cd17b399aac
Nathan S. Evans <evans@in.tum.de>
2011-03-15 11:05:00 +0000

    license and description

--- contrib/peerStartHelper.pl
+++ contrib/peerStartHelper.pl
@@ -1,3 +1,33 @@
+# 
+#  This file is part of GNUnet
+#  (C) 2008, 2009 Christian Grothoff (and other contributing authors)
+# 
+#  GNUnet is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published
+#  by the Free Software Foundation; either version 3, or (at your
+#  option) any later version.
+# 
+#  GNUnet is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with GNUnet; see the file COPYING.  If not, write to the
+#  Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+#  Boston, MA 02111-1307, USA.
+# 
+# 
+# 
+#  @file contrib/peerStartHelper.pl
+#  @brief Helper process for starting gnunet-testing peers.
+#  @author Nathan Evans
+#
+# Finds configuration files (or any files) of the format
+# /path/*/gnunet-testing-config* and runs gnunet-arm with
+# each as the given configuration.
+#
+# usage: peerStartHelper.pl /path/to/testing_dir/
 #!/usr/bin/perl
 use strict;
 
