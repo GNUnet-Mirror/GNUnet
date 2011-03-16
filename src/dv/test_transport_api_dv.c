c1b3e5f6af2f2c4e408988eddfbcb1afbf0ffe81
Nathan S. Evans <evans@in.tum.de>
2011-03-16 11:20:51 +0000

    core api change

--- src/dv/test_transport_api_dv.c
+++ src/dv/test_transport_api_dv.c
@@ -525,6 +525,7 @@ static void connect_notify_peer2 (void *cls,
                   GNUNET_i2s(&pos->peer2->id), GNUNET_h2s (&peer->hashPubKey));
 #endif
       if (NULL == GNUNET_CORE_notify_transmit_ready (pos->peer1handle,
+                                                     GNUNET_YES,
                                                      0,
                                                      TIMEOUT,
                                                      &pos->peer2->id,
