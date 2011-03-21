d6bd47b1206c65e537eb3d3f60a20273e713f993
Matthias Wachs <wachs@net.in.tum.de>
2011-03-21 10:41:54 +0000

    debug and timeout

--- src/transport/test_transport_ats.c
+++ src/transport/test_transport_ats.c
@@ -45,7 +45,7 @@
 /**
  * How long until we give up on transmitting the message?
  */
-#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 5)
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 90)
 
 /**
  * How long until we give up on transmitting the message?
