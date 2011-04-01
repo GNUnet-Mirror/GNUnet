3c03576b2bf8084878c1a21a30544d127775fa26
Matthias Wachs <wachs@net.in.tum.de>
2011-04-01 08:11:53 +0000

    script to execute test with different number of peers

---
+++ src/transport/ats_benchmark.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+start=1
+end=10
+
+if [ ! $1 = "" ]; then
+  start=$1
+fi
+
+if [ ! $2 = "" ]; then
+  end=$2
+fi
+
+for (( c=$start; c<=$end; c++ ))
+do
+  killall -s 31 gnunet-service-arm 2> /dev/null
+  killall -s 31 gnunet-service-transport 2> /dev/null
+  killall -s 31 gnunet-service-statistics 2> /dev/null
+  killall -s 31 gnunet-service-resolver 2> /dev/null
+  killall -s 31 gnunet-service-core 2> /dev/null
+  killall -s 31 gnunet-service-peerinfo 2> /dev/null
+  sleep 1
+  ./test_transport_ats $c
+done
