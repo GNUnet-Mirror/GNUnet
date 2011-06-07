05554f99c40fb24b61ed52c1dcaac2356ed509ea
Christian Grothoff <christian@grothoff.org>
2011-06-07 10:25:27 +0000

    fix

--- src/core/gnunet-service-core.c
+++ src/core/gnunet-service-core.c
@@ -4577,6 +4577,7 @@ cleaning_task (void *cls, const struct GNUNET_SCHEDULER_TaskContext *tc)
   GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
 	      "Core service shutting down.\n");
 #endif
+  GNUNET_assert (transport != NULL);
   GNUNET_TRANSPORT_disconnect (transport);
   transport = NULL;
   GNUNET_CONTAINER_multihashmap_iterate (neighbours,
@@ -4585,7 +4586,6 @@ cleaning_task (void *cls, const struct GNUNET_SCHEDULER_TaskContext *tc)
   GNUNET_CONTAINER_multihashmap_destroy (neighbours);
   neighbours = NULL;
   GNUNET_STATISTICS_set (stats, gettext_noop ("# neighbour entries allocated"), 0, GNUNET_NO);
-  GNUNET_assert (transport != NULL);
   GNUNET_SERVER_notification_context_destroy (notifier);
   notifier = NULL;
   while (NULL != (c = clients))
