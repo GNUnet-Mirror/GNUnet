bdf0e1bba5ae46397fe8f8a037880c290fd0143f
Christian Grothoff <christian@grothoff.org>
2011-08-15 15:02:10 +0000

    check

--- src/util/scheduler.c
+++ src/util/scheduler.c
@@ -1059,6 +1059,7 @@ GNUNET_SCHEDULER_add_continuation (GNUNET_SCHEDULER_Task task,
   void *backtrace_array[50];
 #endif
 
+  GNUNET_assert (NULL != task);
   GNUNET_assert ( (active_task != NULL) ||
 		  (reason == GNUNET_SCHEDULER_REASON_STARTUP) );
   t = GNUNET_malloc (sizeof (struct Task));
