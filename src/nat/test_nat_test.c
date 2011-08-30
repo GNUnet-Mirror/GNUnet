c526fb6819ff203838e5fa2b5905b5d9f6309380
Matthias Wachs <wachs@net.in.tum.de>
2011-08-30 13:23:04 +0000

    check if gnunet-nat-server is correctly installed

--- src/nat/test_nat_test.c
+++ src/nat/test_nat_test.c
@@ -85,6 +85,8 @@ main (int argc, char *const argv[])
   };
   struct GNUNET_OS_Process *gns;
 
+  int nat_res;
+
   char *const argv_prog[] = {
     "test-nat-test",
     "-c",
@@ -105,6 +107,25 @@ main (int argc, char *const argv[])
                     "WARNING",
 #endif
                     NULL);
+
+  nat_res = GNUNET_OS_check_helper_binary ("gnunet-nat-server");
+  if (GNUNET_NO == nat_res)
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_WARNING,
+        "Cannot run NAT test: `%s' %s \n",
+        "gnunet-nat-server",
+         "SUID not set");
+    return 0;
+  }
+  if (GNUNET_SYSERR ==  nat_res)
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_WARNING,
+        "Cannot run NAT test: `%s' %s \n",
+        "gnunet-nat-server",
+        "file not found");
+    return 0;
+  }
+
   gns =
       GNUNET_OS_start_process (NULL, NULL, "gnunet-nat-server",
                                "gnunet-nat-server",
