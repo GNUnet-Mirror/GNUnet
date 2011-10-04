c09e07b428d1d4692b5ad2f51dd77ea3adc13c47
Christian Grothoff <christian@grothoff.org>
2011-10-04 21:15:13 +0000

    use load, not parse

--- src/testing/test_testing_connect.c
+++ src/testing/test_testing_connect.c
@@ -147,9 +147,9 @@ run (void *cls, char *const *args, const char *cfgfile,
   GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "Starting daemon.\n");
 #endif
   c1 = GNUNET_CONFIGURATION_create ();
-  GNUNET_CONFIGURATION_parse (c1, "test_testing_connect_peer1.conf");
+  GNUNET_CONFIGURATION_load (c1, "test_testing_connect_peer1.conf");
   c2 = GNUNET_CONFIGURATION_create ();
-  GNUNET_CONFIGURATION_parse (c2, "test_testing_connect_peer2.conf");
+  GNUNET_CONFIGURATION_load (c2, "test_testing_connect_peer2.conf");
   d1 = GNUNET_TESTING_daemon_start (c1, TIMEOUT, GNUNET_NO, NULL, NULL, 0, NULL,
                                     NULL, NULL, &my_cb1, NULL);
   GNUNET_assert (d1 != NULL);
