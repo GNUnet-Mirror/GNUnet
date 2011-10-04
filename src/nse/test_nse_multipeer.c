2a9710eebee992c9585178793bca604e93b21d3a
Christian Grothoff <christian@grothoff.org>
2011-10-04 21:20:27 +0000

    increase timeout to hopefully fix issues on slow buildbots

--- src/nse/test_nse_multipeer.c
+++ src/nse/test_nse_multipeer.c
@@ -51,7 +51,7 @@ struct NSEPeer *peer_tail;
 /**
  * How long do we run the test?
  */
-#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 30)
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 300)
 
 static int ok;
 
