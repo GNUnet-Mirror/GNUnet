c6c55c8756e73a6560a2513f2071ca6afa3c40eb
Christian Grothoff <christian@grothoff.org>
2011-10-05 08:16:08 +0000

    fix

--- src/util/program.c
+++ src/util/program.c
@@ -222,7 +222,7 @@ GNUNET_PROGRAM_run (int argc, char *const *argv, const char *binaryName,
   {
     GNUNET_CONFIGURATION_destroy (cfg);
     GNUNET_free_non_null (cc.cfgfile);
-    GNUNET_free (loglev);
+    GNUNET_free_non_null (loglev);
     GNUNET_free_non_null (logfile);
     GNUNET_free (allopts);
     GNUNET_free (lpfx);
