0c12ebfc6616bc3932fa2f3ebf459078a2c14f6c
Bart Polot <bart@net.in.tum.de>
2011-10-05 17:31:51 +0000

    Added testcase to check proper working of insertion of own ID into a 2dtorus dht

---
+++ src/dht/test_dht_2dtorus.c
@@ -0,0 +1,524 @@
+/*
+     This file is part of GNUnet.
+     (C) 2011 Christian Grothoff (and other contributing authors)
+
+     GNUnet is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 3, or (at your
+     option) any later version.
+
+     GNUnet is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with GNUnet; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+/**
+ * @file dht/test_dht_2dtorus.c
+ *
+ * @brief Test for the dht service: store and retrieve in a 2d_torus.
+ * Each peer stores it own ID in the DHT and then a different peer tries to
+ * retrieve that key from it. The GET starts after a first round of PUTS has
+ * been made. Periodically, each peer stores its ID into the DHT. If after
+ * a timeout no result has been returned, the test fails.
+ */
+#include "platform.h"
+#include "gnunet_testing_lib.h"
+#include "gnunet_dht_service.h"
+
+#define VERBOSE GNUNET_YES
+#define REMOVE_DIR GNUNET_YES
+
+
+/**
+ * How long until we give up on connecting the peers?
+ */
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 1500)
+
+static int ok;
+
+/**
+ * Be verbose
+ */
+static int verbose;
+
+/**
+ * Total number of peers in the test.
+ */
+static unsigned long long num_peers;
+
+/**
+ * Global configuration file
+ */
+static struct GNUNET_CONFIGURATION_Handle *testing_cfg;
+
+/**
+ * Total number of currently running peers.
+ */
+static unsigned long long peers_running;
+
+/**
+ * Total number of connections in the whole network.
+ */
+static unsigned int total_connections;
+
+/**
+ * The currently running peer group.
+ */
+static struct GNUNET_TESTING_PeerGroup *pg;
+
+/**
+ * File to report results to.
+ */
+static struct GNUNET_DISK_FileHandle *output_file;
+
+/**
+ * File to log connection info, statistics to.
+ */
+static struct GNUNET_DISK_FileHandle *data_file;
+
+/**
+ * Task called to disconnect peers.
+ */
+static GNUNET_SCHEDULER_TaskIdentifier disconnect_task;
+
+/**
+ * Task To perform tests
+ */
+static GNUNET_SCHEDULER_TaskIdentifier test_task;
+
+/**
+ * Task to do DHT_puts
+ */
+static GNUNET_SCHEDULER_TaskIdentifier put_task;
+
+/**
+ * Task called to shutdown test.
+ */
+static GNUNET_SCHEDULER_TaskIdentifier shutdown_handle;
+
+static char *topology_file;
+
+static char *data_filename;
+
+struct GNUNET_TESTING_Daemon *d1;
+
+struct GNUNET_TESTING_Daemon *d2;
+
+struct GNUNET_DHT_Handle **hs;
+
+struct GNUNET_DHT_GetHandle *get_h;
+
+/**
+ * Check whether peers successfully shut down.
+ */
+static void
+shutdown_callback (void *cls, const char *emsg)
+{
+  if (emsg != NULL)
+  {
+#if VERBOSE
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: Shutdown of peers failed!\n");
+#endif
+    ok++;
+  }
+  else
+  {
+#if VERBOSE
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: All peers successfully shut down!\n");
+#endif
+  }
+}
+
+
+static void
+shutdown_task (void *cls, const struct GNUNET_SCHEDULER_TaskContext *tc)
+{
+#if VERBOSE
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: Ending test.\n");
+#endif
+
+  if (disconnect_task != GNUNET_SCHEDULER_NO_TASK)
+  {
+    GNUNET_SCHEDULER_cancel (disconnect_task);
+    disconnect_task = GNUNET_SCHEDULER_NO_TASK;
+  }
+
+  if (data_file != NULL)
+    GNUNET_DISK_file_close (data_file);
+  GNUNET_TESTING_daemons_stop (pg, TIMEOUT, &shutdown_callback, NULL);
+  GNUNET_CONFIGURATION_destroy (testing_cfg);
+}
+
+
+static void
+disconnect_peers (void *cls, const struct GNUNET_SCHEDULER_TaskContext *tc)
+{
+  unsigned int i;
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "test: disconnecting peers\n");
+  disconnect_task = GNUNET_SCHEDULER_NO_TASK;
+  GNUNET_SCHEDULER_cancel (put_task);
+  if (NULL != get_h)
+    GNUNET_DHT_get_stop(get_h);
+  for (i = 0; i < num_peers;  i++)
+  {
+    GNUNET_DHT_disconnect(hs[i]);
+  }
+  GNUNET_SCHEDULER_cancel (shutdown_handle);
+  shutdown_handle = GNUNET_SCHEDULER_add_now(&shutdown_task, NULL);
+}
+
+static void
+dht_get_id_handler (void *cls, struct GNUNET_TIME_Absolute exp,
+                    const GNUNET_HashCode * key,
+                    const struct GNUNET_PeerIdentity *get_path,
+                    unsigned int get_path_length,
+                    const struct GNUNET_PeerIdentity *put_path,
+                    unsigned int put_path_length,
+                    enum GNUNET_BLOCK_Type type, size_t size, const void *data)
+{
+    ok = 0;
+    
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "test: ************* FOUND!!! ***********\n");
+    if (sizeof(GNUNET_HashCode) == size)
+    {
+      const GNUNET_HashCode *h = data;
+      GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                  "test:   Contents: %s\n",
+                  GNUNET_h2s_full (h));
+
+    }
+    GNUNET_SCHEDULER_cancel(disconnect_task);
+    disconnect_task = GNUNET_SCHEDULER_add_now (&disconnect_peers, NULL);
+}
+
+static void
+do_test (void *cls, const struct GNUNET_SCHEDULER_TaskContext *tc)
+{
+  struct GNUNET_TESTING_Daemon *d;
+  struct GNUNET_TESTING_Daemon *o;
+
+  d = GNUNET_TESTING_daemon_get (pg, 2);
+  o = GNUNET_TESTING_daemon_get (pg, 0);
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: test_task\n");
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "test: looking for [2] %s\n",
+              GNUNET_h2s_full (&d->id.hashPubKey));
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "test: from        [0] %s\n",
+              GNUNET_h2s_full (&o->id.hashPubKey));
+  get_h = GNUNET_DHT_get_start (hs[0],
+                                GNUNET_TIME_UNIT_FOREVER_REL, /* timeout */
+                                GNUNET_BLOCK_TYPE_TEST,   /* type */
+                                &d->id.hashPubKey,   /*key to search */
+                                4U,        /* replication level */
+                                GNUNET_DHT_RO_RECORD_ROUTE |
+                                  GNUNET_DHT_RO_DEMULTIPLEX_EVERYWHERE,
+                                NULL,     /* xquery */
+                                0,        /* xquery bits */
+                                &dht_get_id_handler,
+                                NULL);
+  GNUNET_SCHEDULER_cancel (disconnect_task);
+  disconnect_task = GNUNET_SCHEDULER_add_delayed(
+          GNUNET_TIME_relative_multiply(GNUNET_TIME_UNIT_SECONDS, 30),
+                               &disconnect_peers, NULL);
+}
+
+
+static void
+put_id (void *cls, const struct GNUNET_SCHEDULER_TaskContext *tc)
+{
+  struct GNUNET_TESTING_Daemon *d;
+  unsigned int i;
+
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: putting id's in DHT\n");
+  for (i = 0; i < num_peers;  i++)
+  {
+    d = GNUNET_TESTING_daemon_get (pg, i);
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                "test:    putting into DHT: %s\n",
+                GNUNET_h2s_full (&d->id.hashPubKey));
+    GNUNET_DHT_put(hs[i],
+                   &d->id.hashPubKey,
+                   10U,
+                   GNUNET_DHT_RO_RECORD_ROUTE |
+                     GNUNET_DHT_RO_DEMULTIPLEX_EVERYWHERE,
+                    GNUNET_BLOCK_TYPE_TEST,
+                   sizeof(struct GNUNET_PeerIdentity),
+                   (const char *) &d->id,
+                   GNUNET_TIME_UNIT_FOREVER_ABS,
+                   GNUNET_TIME_UNIT_FOREVER_REL,
+                   NULL,
+                   NULL);
+
+  }
+  put_task = GNUNET_SCHEDULER_add_delayed(
+                GNUNET_TIME_relative_multiply(GNUNET_TIME_UNIT_SECONDS, 5),
+                               &put_id, NULL);
+}
+
+
+/**
+ * peergroup_ready: start test when all peers are connected
+ * @param cls closure
+ * @param emsg error message
+ */
+static void
+peergroup_ready (void *cls, const char *emsg)
+{
+  struct GNUNET_TESTING_Daemon *d;
+  char *buf;
+  int buf_len;
+  unsigned int i;
+
+  if (emsg != NULL)
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                "test: Peergroup callback called with error, aborting test!\n");
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                "test: Error from testing: `%s'\n", emsg);
+    ok++;
+    GNUNET_TESTING_daemons_stop (pg, TIMEOUT, &shutdown_callback, NULL);
+    return;
+  }
+#if VERBOSE
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "************************************************************\n");
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "test: Peer Group started successfully!\n");
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+              "test: Have %u connections\n",
+              total_connections);
+#endif
+
+  if (data_file != NULL)
+  {
+    buf = NULL;
+    buf_len = GNUNET_asprintf (&buf, "CONNECTIONS_0: %u\n", total_connections);
+    if (buf_len > 0)
+      GNUNET_DISK_file_write (data_file, buf, buf_len);
+    GNUNET_free (buf);
+  }
+  peers_running = GNUNET_TESTING_daemons_running (pg);
+  
+  GNUNET_assert (peers_running == num_peers);
+  hs = GNUNET_malloc (num_peers * sizeof(struct GNUNET_DHT_Handle *));
+  for (i = 0; i < num_peers;  i++)
+  {
+    d = GNUNET_TESTING_daemon_get (pg, i);
+    hs[i] = GNUNET_DHT_connect(d->cfg, 32);
+  }
+
+  put_task = GNUNET_SCHEDULER_add_now(&put_id, NULL);
+  test_task =
+      GNUNET_SCHEDULER_add_delayed(
+          GNUNET_TIME_relative_multiply(GNUNET_TIME_UNIT_SECONDS, 2),
+          &do_test, NULL);
+  disconnect_task =
+      GNUNET_SCHEDULER_add_delayed (GNUNET_TIME_relative_multiply(
+                                        GNUNET_TIME_UNIT_SECONDS,
+                                        30),
+                                    &disconnect_peers,
+                                    NULL);
+
+}
+
+
+/**
+ * Function that will be called whenever two daemons are connected by
+ * the testing library.
+ *
+ * @param cls closure
+ * @param first peer id for first daemon
+ * @param second peer id for the second daemon
+ * @param distance distance between the connected peers
+ * @param first_cfg config for the first daemon
+ * @param second_cfg config for the second daemon
+ * @param first_daemon handle for the first daemon
+ * @param second_daemon handle for the second daemon
+ * @param emsg error message (NULL on success)
+ */
+static void
+connect_cb (void *cls, const struct GNUNET_PeerIdentity *first,
+            const struct GNUNET_PeerIdentity *second, uint32_t distance,
+            const struct GNUNET_CONFIGURATION_Handle *first_cfg,
+            const struct GNUNET_CONFIGURATION_Handle *second_cfg,
+            struct GNUNET_TESTING_Daemon *first_daemon,
+            struct GNUNET_TESTING_Daemon *second_daemon, const char *emsg)
+{
+//   GNUNET_PEER_Id f;
+//   GNUNET_PEER_Id s;
+
+  if (emsg == NULL)
+  {
+    total_connections++;
+    /*f = */GNUNET_PEER_intern(first);
+    /*s = */GNUNET_PEER_intern(second);
+//   GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: New connection!\n");
+//   GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+//               "test:     %s (%u)\n",
+//               GNUNET_h2s(&first->hashPubKey),
+//               f);
+//   GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+//               "test:     %s (%u)\n",
+//               GNUNET_h2s(&second->hashPubKey),
+//               s);
+  }
+  else
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_DEBUG,
+                "test: Problem with new connection (%s)\n",
+                emsg);
+  }
+
+}
+
+
+/**
+ * run: load configuration options and schedule test to run (start peergroup)
+ * @param cls closure
+ * @param args argv
+ * @param cfgfile configuration file name (can be NULL)
+ * @param cfg configuration handle
+ */
+static void
+run (void *cls, char *const *args, const char *cfgfile,
+     const struct GNUNET_CONFIGURATION_Handle *cfg)
+{
+  char *temp_str;
+  struct GNUNET_TESTING_Host *hosts;
+
+  ok = 1;
+  testing_cfg = GNUNET_CONFIGURATION_dup (cfg);
+
+  GNUNET_log_setup ("test_dht_2dtorus",
+#if VERBOSE
+                    "DEBUG",
+#else
+                    "WARNING",
+#endif
+                    NULL);
+
+#if VERBOSE
+  GNUNET_log (GNUNET_ERROR_TYPE_DEBUG, "test: Starting daemons.\n");
+  GNUNET_CONFIGURATION_set_value_string (testing_cfg, "testing",
+                                         "use_progressbars", "YES");
+#endif
+
+  if (GNUNET_OK !=
+      GNUNET_CONFIGURATION_get_value_number (testing_cfg, "testing",
+                                             "num_peers", &num_peers))
+  {
+    GNUNET_assert (GNUNET_OK ==
+                   GNUNET_CONFIGURATION_load (testing_cfg,
+                                              "test_dht_2dtorus.conf"));
+    if (GNUNET_OK !=
+        GNUNET_CONFIGURATION_get_value_number (testing_cfg, "testing",
+                                               "num_peers", &num_peers))
+    {
+      GNUNET_log (GNUNET_ERROR_TYPE_ERROR,
+                  "Option TESTING:NUM_PEERS is required!\n");
+      return;
+    }
+  }
+
+  if (GNUNET_OK !=
+      GNUNET_CONFIGURATION_get_value_string (testing_cfg, "testing",
+                                             "topology_output_file",
+                                             &topology_file))
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_ERROR,
+                "Option test_dht_2d:topology_output_file is required!\n");
+    return;
+  }
+
+  if (GNUNET_OK !=
+      GNUNET_CONFIGURATION_get_value_string (testing_cfg, "test_dht_2dtorus",
+                                             "data_output_file",
+                                             &data_filename))
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_ERROR,
+                "Option test_dht_2d:data_output_file is required!\n");
+    return;
+  }
+
+  data_file =
+      GNUNET_DISK_file_open (data_filename,
+                             GNUNET_DISK_OPEN_READWRITE |
+                             GNUNET_DISK_OPEN_CREATE,
+                             GNUNET_DISK_PERM_USER_READ |
+                             GNUNET_DISK_PERM_USER_WRITE);
+  if (data_file == NULL)
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_WARNING, "Failed to open %s for output!\n",
+                data_filename);
+    GNUNET_free (data_filename);
+  }
+
+  if (GNUNET_YES ==
+      GNUNET_CONFIGURATION_get_value_string (cfg, "test_dht_2dtorus",
+                                             "output_file", &temp_str))
+  {
+    output_file =
+        GNUNET_DISK_file_open (temp_str,
+                               GNUNET_DISK_OPEN_READWRITE |
+                               GNUNET_DISK_OPEN_CREATE,
+                               GNUNET_DISK_PERM_USER_READ |
+                               GNUNET_DISK_PERM_USER_WRITE);
+    if (output_file == NULL)
+      GNUNET_log (GNUNET_ERROR_TYPE_WARNING, "Failed to open %s for output!\n",
+                  temp_str);
+  }
+  GNUNET_free_non_null (temp_str);
+
+  hosts = GNUNET_TESTING_hosts_load (testing_cfg);
+
+  pg = GNUNET_TESTING_peergroup_start (testing_cfg, num_peers, TIMEOUT,
+                                       &connect_cb, &peergroup_ready, NULL,
+                                       hosts);
+  GNUNET_assert (pg != NULL);
+  shutdown_handle =
+      GNUNET_SCHEDULER_add_delayed (GNUNET_TIME_relative_get_forever (),
+                                    &shutdown_task, NULL);
+}
+
+
+
+/**
+ * test_dht_2d command line options
+ */
+static struct GNUNET_GETOPT_CommandLineOption options[] = {
+  {'V', "verbose", NULL,
+   gettext_noop ("be verbose (print progress information)"),
+   0, &GNUNET_GETOPT_set_one, &verbose},
+  GNUNET_GETOPT_OPTION_END
+};
+
+
+/**
+ * Main: start test
+ */
+int
+main (int argc, char *argv[])
+{
+  GNUNET_PROGRAM_run (argc, argv, "test_dht_2dtorus",
+                      gettext_noop ("Test dht in a small 2D torus."), options,
+                      &run, NULL);
+#if REMOVE_DIR
+  GNUNET_DISK_directory_remove ("/tmp/test_dht_2dtorus");
+#endif
+  if (0 != ok)
+  {
+    GNUNET_log (GNUNET_ERROR_TYPE_WARNING, "test: FAILED!\n");
+  }
+  return ok;
+}
+
+/* end of test_dht_2dtorus.c */
