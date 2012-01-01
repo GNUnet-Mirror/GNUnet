3e73780b201b600dd8dd8725f5a628d62d9c4f51
Christian Grothoff <christian@grothoff.org>
2012-01-01 23:10:37 +0000

    -copyright

--- src/dns/dnsparser.c
+++ src/dns/dnsparser.c
@@ -1,3 +1,29 @@
+/*
+      This file is part of GNUnet
+      (C) 2010, 2011, 2012 Christian Grothoff (and other contributing authors)
+
+      GNUnet is free software; you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published
+      by the Free Software Foundation; either version 2, or (at your
+      option) any later version.
+
+      GNUnet is distributed in the hope that it will be useful, but
+      WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+      General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with GNUnet; see the file COPYING.  If not, write to the
+      Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+      Boston, MA 02111-1307, USA.
+ */
+
+/**
+ * @file dns/dnsparser.c
+ * @brief helper library to parse DNS packets. 
+ * @author Philipp Toelke
+ */
+
 #include "platform.h"
 #include "gnunet_dnsparser_lib.h"
 
