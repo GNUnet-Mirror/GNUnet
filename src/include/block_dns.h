f5bee1c9c607cf39a3c22f9c8a7f52db447c1ee9
Christian Grothoff <christian@grothoff.org>
2012-01-02 10:43:06 +0000

    -copyright

--- src/include/block_dns.h
+++ src/include/block_dns.h
@@ -1,3 +1,27 @@
+/*
+     This file is part of GNUnet.
+     (C) 2009 Christian Grothoff (and other contributing authors)
+
+     GNUnet is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published
+     by the Free Software Foundation; either version 3, or (at your
+     option) any later version.
+
+     GNUnet is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with GNUnet; see the file COPYING.  If not, write to the
+     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+     Boston, MA 02111-1307, USA.
+*/
+
+/**
+ * @file include/block_dns.h
+ * @author Philipp Toelke
+ */
 #ifndef _GNVPN_BLOCKDNS_H_
 #define _GNVPN_BLOCKDNS_H_
 
