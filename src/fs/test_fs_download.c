6142bbc4d7c388a2fc73415c466fcbdbecd09c61
Christian Grothoff <christian@grothoff.org>
2012-02-22 08:46:38 +0000

    -increase timeout to help sparc bot

--- src/fs/test_fs_download.c
+++ src/fs/test_fs_download.c
@@ -42,7 +42,7 @@
 /**
  * How long until we give up on transmitting the message?
  */
-#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 60)
+#define TIMEOUT GNUNET_TIME_relative_multiply (GNUNET_TIME_UNIT_SECONDS, 120)
 
 /**
  * How long should our test-content live?
