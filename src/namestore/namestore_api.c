f8676628e5fb812d68e1468dbc6d115dc641ffbf
Matthias Wachs <wachs@net.in.tum.de>
2012-03-14 12:45:35 +0000

    - adding assertion for name

--- src/namestore/namestore_api.c
+++ src/namestore/namestore_api.c
@@ -477,6 +477,7 @@ handle_zone_to_name_response (struct GNUNET_NAMESTORE_QueueEntry *qe,
     expire = GNUNET_TIME_absolute_ntoh(msg->expire);
 
     name_tmp = (char *) &msg[1];
+    GNUNET_assert ('\0' == name_tmp[name_len]);
     rd_tmp = &name_tmp[name_len];
 
     struct GNUNET_NAMESTORE_RecordData rd[rd_count];
