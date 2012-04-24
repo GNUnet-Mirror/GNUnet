8025c06ca58905eb098b43fb3cd606e341d418d7
Christian Grothoff <christian@grothoff.org>
2012-04-24 11:59:44 +0000

    -fix

--- src/datastore/perf_datastore_api.c
+++ src/datastore/perf_datastore_api.c
@@ -285,8 +285,9 @@ run_tests (void *cls, int success, struct GNUNET_TIME_Absolute min_expiration, c
   if (success != GNUNET_YES)
   {
     FPRINTF (stderr,
-             "Test 'put' operation failed with error `%s' database likely not setup, skipping test.",
+             "Test 'put' operation failed with error `%s' database likely not setup, skipping test.\n",
              msg);
+    GNUNET_DATASTORE_disconnect (datastore, GNUNET_YES);
     GNUNET_free (crc);
     return;
   }
