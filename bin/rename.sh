959ab92452c48a0d5f49409dd1b285f422703c91
Christian Grothoff <christian@grothoff.org>
2012-05-27 19:51:26 +0000

    -undo

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for n in `find * -name "*.c"` `find * -name "*.h"` `find * -name "*.in"`
+for n in `find * -name "*.c"` `find * -name "*.h"`
 do
  cat $n | sed -e "s/$1/$2/g" > $n.new
  mv $n.new $n || echo "Move failed: $n.new to $n"
