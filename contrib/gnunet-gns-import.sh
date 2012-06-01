599fdbabddfa5c2047fd28e341ccdfdc6353d06b
Christian Grothoff <christian@grothoff.org>
2012-06-01 19:41:58 +0000

    -adding GNS fcfs import script

---
+++ contrib/gnunet-gns-import.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# This shell-script will import some GNS authorities into your GNS
+# namestore.
+gnunet-namestore -a -e never -n fcfs -p -t PKEY -V 72QC35CO20UJN1E91KPJFNT9TG4CLKAPB4VK9S3Q758S9MLBRKOG
