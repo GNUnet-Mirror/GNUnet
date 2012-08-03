2aab65fd2faa1a83a64888c79813b8cb646a1f77
Bart Polot <bart@net.in.tum.de>
2012-08-03 14:00:04 +0000

    - fix installscript for sudo when root

--- src/gns/nss/install-nss-plugin.sh
+++ src/gns/nss/install-nss-plugin.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-# $1 - sudo binary (empty if root)
-# $2 - shell
-# $3 - top_builddir
-# $4 - nssdir
-if [ "`whoami`" = "root" ]; then
-  $1 $2/libtool --finish $3
-  rm -f $3/libnss_gns.la $3/libnss_gns4.la $3/libnss_gns6.la
-else
-  $1 $2 $3/libtool --finish $4
-  $1 rm -f $4/libnss_gns.la $4/libnss_gns4.la $4/libnss_gns6.la
-fi
+# $1 - shell
+# $2 - top_builddir
+# $3 - nssdir
+# $4 - sudo binary (empty if root)
+$4 $1 $2/libtool --finish $3
+$4 rm -f $3/libnss_gns.la $3/libnss_gns4.la $3/libnss_gns6.la
+
 
