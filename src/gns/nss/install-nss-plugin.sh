042343d4ea2fa5dc1380ee92b4f2808ef0028843
Nathan S. Evans <evans@in.tum.de>
2012-08-03 13:15:19 +0000

    - Fix script for buildbots that run as root (no sudo provided)

--- src/gns/nss/install-nss-plugin.sh
+++ src/gns/nss/install-nss-plugin.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# $1 - sudo binary
+# $1 - sudo binary (empty if root)
 # $2 - shell
 # $3 - top_builddir
 # $4 - nssdir
-$1 $2 $3/libtool --finish $4
-$1 rm -f $4/libnss_gns.la $4/libnss_gns4.la $4/libnss_gns6.la 
+if [ "`whoami`" = "root" ]; then
+  $1 $2/libtool --finish $3
+  rm -f $3/libnss_gns.la $3/libnss_gns4.la $3/libnss_gns6.la
+else
+  $1 $2 $3/libtool --finish $4
+  $1 rm -f $4/libnss_gns.la $4/libnss_gns4.la $4/libnss_gns6.la
+fi
+
