673effab80be5340cb0b5f37f8efb48e1c9824f2
Christian Grothoff <christian@grothoff.org>
2012-09-27 12:54:34 +0000

    -check return value

--- src/util/gnunet-rsa.c
+++ src/util/gnunet-rsa.c
@@ -126,7 +126,11 @@ create_keys (const char *fn)
     GNUNET_CRYPTO_hash (&make_keys, sizeof (make_keys), &h2);
     GNUNET_CRYPTO_hash (&hc, sizeof (hc), &h3);
     GNUNET_CRYPTO_hash_xor (&h2, &h3, &hc);
-    pk = GNUNET_CRYPTO_rsa_key_create_from_hash (&hc);
+    if (NULL == (pk = GNUNET_CRYPTO_rsa_key_create_from_hash (&hc))
+    {	
+       GNUNET_break (0);
+       break;
+    }
     enc = GNUNET_CRYPTO_rsa_encode_key (pk);
     if (htons (enc->len) != fwrite (enc, 1, htons (enc->len), f))
       {
