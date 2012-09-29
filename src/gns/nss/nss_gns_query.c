ae2c79e76ba294b1132b44230aec3c456ef83824
Christian Grothoff <christian@grothoff.org>
2012-09-29 22:29:39 +0000

    -fixing style issue

--- src/gns/nss/nss_gns_query.c
+++ src/gns/nss/nss_gns_query.c
@@ -33,70 +33,74 @@
  * @param u the userdata (result struct)
  * @return -1 on error else 0
  */
-int gns_resolve_name(int af, const char *name, struct userdata *u)
+int 
+gns_resolve_name (int af,
+		  const char *name, 
+		  struct userdata *u)
 {
   FILE *p;
   char *cmd;
   char line[128];
 
-  if (af == AF_INET6)
+  if (AF_INET6 == af)
   {
-    if (-1 == asprintf(&cmd, "%s -t AAAA -u %s\n", "gnunet-gns -r", name))
+    if (-1 == asprintf (&cmd, 
+			"%s -t AAAA -u %s\n", 
+			"gnunet-gns -r", name))
       return -1;
   }
   else
   {
-    if (-1 == asprintf(&cmd, "%s %s\n", "gnunet-gns -r -u", name))
+    if (-1 == asprintf (&cmd, 
+			"%s %s\n", 
+			"gnunet-gns -r -u", name))
       return -1;
   }
-
-  p = popen(cmd,"r");
-
-  if (p != NULL )
+  if (NULL == (p = popen (cmd, "r")))
   {
-    while (fgets( line, sizeof(line), p ) != NULL)
+    free (cmd);
+    return -1;
+  }
+  while (NULL != fgets (line, sizeof(line), p))
+  {
+    if (u->count >= MAX_ENTRIES)
+      break;
+    if (line[strlen(line)-1] == '\n')
     {
-
-      if (u->count >= MAX_ENTRIES)
-        break;
-
-      if (line[strlen(line)-1] == '\n')
+      line[strlen(line)-1] = '\0';
+      if (AF_INET == af)
       {
-        line[strlen(line)-1] = '\0';
-        if (af == AF_INET)
+	if (inet_pton(af, line, &(u->data.ipv4[u->count])))
         {
-          if (inet_pton(af, line, &(u->data.ipv4[u->count])))
-          {
-            u->count++;
-            u->data_len += sizeof(ipv4_address_t);
-          }
-          else
-          {
-            fclose (p);
-            free (cmd);
-            return -1;
-          }
-        }
-        else if ((af == AF_INET6))
+	  u->count++;
+	  u->data_len += sizeof(ipv4_address_t);
+	}
+	else
+	{
+	  fclose (p);
+	  free (cmd);
+	  return -1;
+	}
+      }
+      else if (AF_INET6 == af)
+      {
+	if (inet_pton(af, line, &(u->data.ipv6[u->count])))
         {
-          if (inet_pton(af, line, &(u->data.ipv6[u->count])))
-          {
-            u->count++;
-            u->data_len += sizeof(ipv6_address_t);
-          }
-          else
-          {
-            fclose (p);
-            free (cmd);
-            return -1;
-          }
-        }
+	  u->count++;
+	  u->data_len += sizeof(ipv6_address_t);
+	}
+	else
+        {
+	  fclose (p);
+	  free (cmd);
+	  return -1;
+	}
       }
     }
-    fclose (p);
   }
+  fclose (p);
   free (cmd);
-
   return 0;
-
 }
+
+/* end of nss_gns_query.c */
