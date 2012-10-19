6a73109cec17bc4ed3101e7793bbb08ddd93606d
Sree Harsha Totakura <totakura@in.tum.de>
2012-10-19 13:56:08 +0000

    reverted to version 24399

--- src/testbed/test_testbed_api_topology.c
+++ src/testbed/test_testbed_api_topology.c
@@ -138,10 +138,8 @@ test_master (void *cls, unsigned int num_peers,
 						  GNUNET_TESTBED_TOPOLOGY_ERDOS_RENYI,
                                                   NUM_PEERS);
   GNUNET_assert (NULL != op);
-  shutdown_task = GNUNET_SCHEDULER_add_delayed (//GNUNET_TIME_relative_multiply
-                                                //(GNUNET_TIME_UNIT_SECONDS,
-                                                //120),
-                                                GNUNET_TIME_UNIT_FOREVER_REL,
+  shutdown_task = GNUNET_SCHEDULER_add_delayed (GNUNET_TIME_relative_multiply
+                                                (GNUNET_TIME_UNIT_SECONDS, 120),
                                                 do_shutdown, NULL);
 }
 
