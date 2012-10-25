a3840acef308e54efa2a33180d47a0772c12919b
Christian Grothoff <christian@grothoff.org>
2012-10-25 15:02:29 +0000

    -check that which succeeded

--- src/dns/test_gnunet_dns.sh
+++ src/dns/test_gnunet_dns.sh
@@ -6,9 +6,18 @@ then
   echo "This test only works if run as root.  Skipping."
   exit 0
 fi
+if ! which sudo > /dev/null 
+then
+  echo "This test requires sudo.  Skipping."
+fi
 if [ ! -x `which sudo` ]
 then
   echo "This test requires sudo.  Skipping."
+  exit 0
+fi
+if ! which nslookup
+then 
+  echo "This test requires nslookup.  Skipping."
   exit 0
 fi
 if [ ! -x `which nslookup` ]
