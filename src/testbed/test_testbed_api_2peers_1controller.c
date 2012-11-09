b1c510a48148da5b7f1ebc808290a6ecd61f0cb7
Sree Harsha Totakura <totakura@in.tum.de>
2012-11-09 15:13:40 +0000

    -reverted to allow cleanup

--- src/testbed/test_testbed_api_2peers_1controller.c
+++ src/testbed/test_testbed_api_2peers_1controller.c
@@ -176,8 +176,8 @@ static enum Stage result;
         GNUNET_SCHEDULER_cancel (abort_task);                   \
       abort_task = GNUNET_SCHEDULER_NO_TASK;                    \
       GNUNET_SCHEDULER_add_now (do_shutdown, NULL);             \
-      exit (1);                                                 \
-    }                                                           \
+      return;                                                   \
+    }                                                          \
   } while (0)
 
 /**
