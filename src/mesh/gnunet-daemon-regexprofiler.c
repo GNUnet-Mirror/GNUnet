781ecece3a930b3d2a1a33ad9ed2bb590c010fc2
Christian Grothoff <christian@grothoff.org>
2012-11-14 12:28:36 +0000

    -delay between peer start and regex PUTs

--- src/mesh/gnunet-daemon-regexprofiler.c
+++ src/mesh/gnunet-daemon-regexprofiler.c
@@ -291,7 +291,7 @@ main (int argc, char *const *argv)
 
   if (GNUNET_OK != GNUNET_STRINGS_get_utf8_args (argc, argv, &argc, &argv))
     return 2;
-
+  sleep (60);
   return (GNUNET_OK ==
           GNUNET_PROGRAM_run (argc, argv, "regexprofiler",
                               gettext_noop
