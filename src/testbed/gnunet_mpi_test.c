658bb6d4f307649b3403fa560816ba985fececc8
Sree Harsha Totakura <totakura@in.tum.de>
2012-12-07 17:57:48 +0000

    - fork within MPI

--- src/testbed/gnunet_mpi_test.c
+++ src/testbed/gnunet_mpi_test.c
@@ -2,11 +2,18 @@
 #include "gnunet_util_lib.h"
 #include <mpi.h>
 
+/**
+ * Generic logging shorthand
+ */
+#define LOG(kind,...)                                           \
+  GNUNET_log_from (kind, "gnunet-mpi-test", __VA_ARGS__)
+
 int main (int argc, char *argv[])
 {
   char *msg;
   char *filename;
   pid_t pid;
+  pid_t ppid;
   int ntasks;
   int rank;
   int msg_size;
@@ -19,8 +26,10 @@ int main (int argc, char *argv[])
     goto finalize;
   if (MPI_SUCCESS != MPI_Comm_rank (MPI_COMM_WORLD, &rank))
     goto finalize;
-  (void) GNUNET_asprintf (&filename, "%d.mpiout", rank);
+  pid = getpid();
+  (void) GNUNET_asprintf (&filename, "%d-%d.mpiout", (int) pid, rank);
   msg_size = GNUNET_asprintf (&msg, "My rank is: %d\n", rank);
+  printf ("%s", msg);
   if (msg_size == GNUNET_DISK_fn_write (filename,
                                         msg, msg_size,
                                         GNUNET_DISK_PERM_USER_READ
@@ -30,6 +39,57 @@ int main (int argc, char *argv[])
     ret = GNUNET_OK;
   GNUNET_free (filename);
   GNUNET_free (msg);
+  if (GNUNET_OK != ret)
+    goto finalize;
+
+  ret = GNUNET_SYSERR;
+  ppid = pid;
+  pid = fork ();
+  if (-1 == pid)
+  {
+    GNUNET_log_strerror (GNUNET_ERROR_TYPE_ERROR, "fork");
+    goto finalize;
+  }
+  if (0 == pid)
+  {
+    /* child code */
+    pid = getpid ();
+    (void) GNUNET_asprintf (&filename, "%d-%d.mpiout", (int) pid, rank);
+    msg_size = GNUNET_asprintf (&msg, "Child of %d\n", (int) ppid);
+    printf ("%s", msg);
+    if (msg_size == GNUNET_DISK_fn_write (filename,
+                                        msg, msg_size,
+                                        GNUNET_DISK_PERM_USER_READ
+                                        | GNUNET_DISK_PERM_GROUP_READ
+                                        | GNUNET_DISK_PERM_USER_WRITE
+                                        | GNUNET_DISK_PERM_GROUP_WRITE))
+    ret = GNUNET_OK;
+    GNUNET_free (filename);
+    GNUNET_free (msg);
+    return (GNUNET_OK == ret) ? 0 : 1;
+  }
+  else {
+    int status;
+    int childpid;
+
+    childpid = waitpid (pid, &status, 0);
+    if (childpid != pid)
+    {
+      GNUNET_break (0);
+      goto finalize;
+    }
+    if (!WIFEXITED (status))
+    {
+      GNUNET_break (0);
+      goto finalize;
+    }
+    if (0 != WEXITSTATUS (status))
+    {
+      GNUNET_break (0);
+    }
+  }
+  ret = GNUNET_OK;
+
  finalize:
   (void) MPI_Finalize ();
   return (GNUNET_OK == ret) ? 0 : 1;
