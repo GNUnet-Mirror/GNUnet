bce99a1da47ff3cc2badd21b0caadb808ee46191
Sree Harsha Totakura <totakura@in.tum.de>
2013-02-27 16:58:02 +0000

    script to prepare tree for pre-ecc testbed compilation

---
+++ contrib/testbed_setup_pre_ecc_tree.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+svn up -r26032 
+svn up -r26167 src/nse/ src/regex/
+svn up -r26079 src/include/gnunet_helper_lib.h src/util/helper.c
+svn up -r26219 src/include/gnunet_protocols.h
+svn up src/include/gnunet_testbed_service.h src/testbed/
+svn up contrib
