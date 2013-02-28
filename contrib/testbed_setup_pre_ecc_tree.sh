637fd90be46e5dad3ef3b2beffa78551e63e2722
Sree Harsha Totakura <totakura@in.tum.de>
2013-02-28 13:28:04 +0000

    update configure.ac to HEAD

--- contrib/testbed_setup_pre_ecc_tree.sh
+++ contrib/testbed_setup_pre_ecc_tree.sh
@@ -6,3 +6,4 @@ svn up -r26079 src/include/gnunet_helper_lib.h src/util/helper.c
 svn up -r26219 src/include/gnunet_protocols.h
 svn up src/include/gnunet_testbed_service.h src/testbed/
 svn up contrib
+svn up configure.ac
