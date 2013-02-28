fc19b9680d0e041420cee7ebe2e82a3957f786df
Bart Polot <bart@net.in.tum.de>
2013-02-28 13:37:01 +0000

    - keep regex to HEAD

--- contrib/testbed_setup_pre_ecc_tree.sh
+++ contrib/testbed_setup_pre_ecc_tree.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 svn up -r26032 
-svn up -r26167 src/nse/ src/regex/
+svn up -r26167 src/nse/
 svn up -r26079 src/include/gnunet_helper_lib.h src/util/helper.c
 svn up -r26219 src/include/gnunet_protocols.h
-svn up src/include/gnunet_testbed_service.h src/testbed/
+svn up src/include/gnunet_testbed_service.h src/testbed/ src/regex/
 svn up contrib
 svn up configure.ac
