35b6a4449bba6b62831736278a4d6af718bbf424
Bart Polot <bart@net.in.tum.de>
2013-03-01 11:14:00 +0000

    - keep dht up to date

--- contrib/testbed_setup_pre_ecc_tree.sh
+++ contrib/testbed_setup_pre_ecc_tree.sh
@@ -4,6 +4,6 @@ svn up -r26032
 svn up -r26167 src/nse/
 svn up -r26079 src/include/gnunet_helper_lib.h src/util/helper.c
 svn up -r26219 src/include/gnunet_protocols.h
-svn up src/include/gnunet_testbed_service.h src/testbed/ src/regex/
+svn up src/include/gnunet_testbed_service.h src/testbed/ src/regex/ src/dht/
 svn up contrib
 svn up configure.ac
