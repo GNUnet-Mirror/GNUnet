ead89b2005376011111abab67088ed5cb475e200
Sree Harsha Totakura <totakura@in.tum.de>
2013-04-16 12:42:44 +0000

    - revert to using memory mapped host keys file

--- src/testing/testing.c
+++ src/testing/testing.c
@@ -92,6 +92,16 @@ struct GNUNET_TESTING_System
   char *hostkeys_data;
 
   /**
+   * memory map for 'hostkeys_data'.
+   */
+  struct GNUNET_DISK_MapHandle *map;
+
+  /**
+   * File descriptor for the map.
+   */
+  struct GNUNET_DISK_FileHandle *map_fd;
+
+  /**
    * Bitmap where each TCP port that has already been reserved for
    * some GNUnet peer is recorded.  Note that we additionally need to
    * test if a port is already in use by non-GNUnet components before
@@ -262,15 +272,19 @@ hostkeys_load (struct GNUNET_TESTING_System *system)
     GNUNET_free (filename);
     return GNUNET_SYSERR;
   }
-  system->hostkeys_data = GNUNET_malloc_large ((size_t) fs);
-  if (GNUNET_DISK_fn_read (filename, system->hostkeys_data, fs) != (ssize_t) fs)
+  system->map_fd = GNUNET_DISK_file_open (filename, GNUNET_DISK_OPEN_READ,
+					 GNUNET_DISK_PERM_NONE);
+  if (NULL == system->map_fd)
   {
+    GNUNET_log_strerror_file (GNUNET_ERROR_TYPE_ERROR, "open", filename);
     GNUNET_free (filename);
-    GNUNET_free (system->hostkeys_data);
-    system->hostkeys_data = NULL;
     return GNUNET_SYSERR;
   }
   system->total_hostkeys = fs / GNUNET_TESTING_HOSTKEYFILESIZE;
+  system->hostkeys_data = GNUNET_DISK_file_map (system->map_fd,
+						&system->map,
+						GNUNET_DISK_MAP_TYPE_READ,
+						fs);
   GNUNET_free (filename);
   return GNUNET_OK;
 }
@@ -285,7 +299,11 @@ static void
 hostkeys_unload (struct GNUNET_TESTING_System *system)
 {
   GNUNET_break (NULL != system->hostkeys_data);
-  GNUNET_free_non_null (system->hostkeys_data);
+  system->hostkeys_data = NULL;
+  GNUNET_DISK_file_unmap (system->map);
+  system->map = NULL;
+  GNUNET_DISK_file_close (system->map_fd);
+  system->map_fd = NULL;
   system->hostkeys_data = NULL;
   system->total_hostkeys = 0;
 }
