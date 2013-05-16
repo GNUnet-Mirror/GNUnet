39963bb835382e3de74f82d671afa4213b703fdc
Matthias Wachs <wachs@net.in.tum.de>
2013-05-16 13:02:52 +0000

    returning here is not a good idea since: - IPv6 socket can be opened but will be not closed if IPv4 error is handled as hard error - Plugin will return correctly with IPv6 socket open but never scheduled

--- src/transport/plugin_transport_udp.c
+++ src/transport/plugin_transport_udp.c
@@ -2515,6 +2515,7 @@ setup_sockets (struct Plugin *plugin,
     plugin->sockv6 = GNUNET_NETWORK_socket_create (PF_INET6, SOCK_DGRAM, 0);
     if (NULL == plugin->sockv6)
     {
+    	GNUNET_log_strerror (GNUNET_ERROR_TYPE_WARNING, "socket");
       LOG (GNUNET_ERROR_TYPE_WARNING, "Disabling IPv6 since it is not supported on this system!\n");
       plugin->enable_ipv6 = GNUNET_NO;
     }
@@ -2588,7 +2589,8 @@ setup_sockets (struct Plugin *plugin,
   if (NULL == plugin->sockv4)
   {
     GNUNET_log_strerror (GNUNET_ERROR_TYPE_WARNING, "socket");
-    return sockets_created;
+    LOG (GNUNET_ERROR_TYPE_WARNING, "Disabling IPv4 since it is not supported on this system!\n");
+    plugin->enable_ipv4 = GNUNET_NO;
   }
   else
   {
