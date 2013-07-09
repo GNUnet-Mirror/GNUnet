8ab50828392b65dbbb0d5eb976276f1cff6c4ddb
Bart Polot <bart@net.in.tum.de>
2013-07-09 23:46:20 +0000

    - fix script

--- src/mesh/beautify_log.sh
+++ src/mesh/beautify_log.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-cat log | grep "Mesh for peer" > __tmp_peers
+cp log .log
+cat .log | grep "Mesh for peer" > __tmp_peers
 cat __tmp_peers | while read line; do
     PEER=`echo $line | sed -e 's/.*\[\(....\)\].*/\1/'`
     PID=`echo $line | sed -e 's/.*mesh-\([0-9]*\).*/\1/'`
     echo "$PID => $PEER"
-    cat log | sed -e "s/mesh-$PID/MESH $PEER/" > .log
+    cat .log | sed -e "s/mesh-$PID/MESH $PEER/" > .log2
+    mv .log2 .log
 done 
 
 rm __tmp_peers
