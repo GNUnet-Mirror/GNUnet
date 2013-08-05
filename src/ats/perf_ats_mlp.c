6311b1d73410f4a95c1fa634de11e1b22019e38d
Matthias Wachs <wachs@net.in.tum.de>
2013-08-05 12:38:38 +0000

    duplicate header

--- src/ats/perf_ats_mlp.c
+++ src/ats/perf_ats_mlp.c
@@ -24,33 +24,6 @@
  * @author Matthias Wachs
 
  */
-
-/*
-     This file is part of GNUnet.
-     (C) 2010,2011 Christian Grothoff (and other contributing authors)
-
-     GNUnet is free software; you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published
-     by the Free Software Foundation; either version 3, or (at your
-     option) any later version.
-
-     GNUnet is distributed in the hope that it will be useful, but
-     WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-     General Public License for more details.
-
-     You should have received a copy of the GNU General Public License
-     along with GNUnet; see the file COPYING.  If not, write to the
-     Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-     Boston, MA 02111-1307, USA.
-*/
-/**
- * @file ats/test_ats_mlp.c
- * @brief basic test for the MLP solver
- * @author Christian Grothoff
- * @author Matthias Wachs
-
- */
 #include "platform.h"
 #include "gnunet_util_lib.h"
 #include "gnunet_statistics_service.h"
