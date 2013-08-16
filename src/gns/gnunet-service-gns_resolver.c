37284be800eef44babef8fc3cc0461c0d3cbea67
LRN <lrn1986@gmail.com>
2013-08-16 04:08:14 +0000

    Fix the lack of memrchr on W32

--- src/gns/gnunet-service-gns_resolver.c
+++ src/gns/gnunet-service-gns_resolver.c
@@ -1324,6 +1324,18 @@ fail_resolution (void *cls,
   GNS_resolver_lookup_cancel (rh);
 }
 
+#ifdef WINDOWS
+/* Don't have this on W32, here's a naive implementation */
+void *memrchr (const void *s, int c, size_t n)
+{
+  size_t i;
+  unsigned char *ucs = (unsigned char *) s;
+  for (i = n - 1; i >= 0; i--)
+    if (ucs[i] == c)
+      return (void *) &ucs[i];
+  return NULL;
+}
+#endif
 
 /**
  * Get the next, rightmost label from the name that we are trying to resolve,
