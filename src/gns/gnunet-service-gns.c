2cb19e37aa832394606af654199881f323f69dc8
Christian Grothoff <christian@grothoff.org>
2013-08-22 07:34:41 +0000

    -this header is not used here

--- src/gns/gnunet-service-gns.c
+++ src/gns/gnunet-service-gns.c
@@ -25,7 +25,6 @@
  */
 #include "platform.h"
 #include "gnunet_util_lib.h"
-#include "gnunet_transport_service.h"
 #include "gnunet_dns_service.h"
 #include "gnunet_dnsparser_lib.h"
 #include "gnunet_dht_service.h"
