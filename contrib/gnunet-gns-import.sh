e097895ea54b316c3ede8640f6dd49f864313a2d
Christian Grothoff <christian@grothoff.org>
2013-09-05 11:15:34 +0000

    -update default zone initialization (#3009)

--- contrib/gnunet-gns-import.sh
+++ contrib/gnunet-gns-import.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 # This shell-script will import some GNS authorities into your GNS
 # namestore.
-gnunet-namestore -a -e never -n fcfs -p -t PKEY -V 72QC35CO20UJN1E91KPJFNT9TG4CLKAPB4VK9S3Q758S9MLBRKOG
+#
+# By default, we create three GNS zones:
+gnunet-identity -C master-zone
+gnunet-identity -C short-zone
+gnunet-identity -C private-zone
+
+# Integrate those with the respective subsystems.
+gnunet-identity -e short-zone -s gns-short
+gnunet-identity -e master-zone -s gns-master
+gnunet-identity -e private-zone -s gns-private
+
+MASTER=`gnunet-identity -d | grep master-zone | awk '{print $3}`
+SHORT=`gnunet-identity -d | grep short-zone | awk '{print $3}`
+PRIVATE=`gnunet-identity -d | grep private-zone | awk '{print $3}`
+
+# Link short and private zones into master zone
+gnunet-namestore -z master-zone -a -e never -n private -p -t PKEY -V $PRIVATE 
+gnunet-namestore -z master-zone -a -e never -n short -p -t PKEY -V $SHORT 
+
+# Link GNUnet's FCFS zone into master zone
+gnunet-namestore -z master-zone -a -e never -n fcfs -p -t PKEY -V 72QC35CO20UJN1E91KPJFNT9TG4CLKAPB4VK9S3Q758S9MLBRKOG
+
