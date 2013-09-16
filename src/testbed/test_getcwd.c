82ef710475746a3365e0185e52137e3034532425
Sree Harsha Totakura <totakura@in.tum.de>
2013-09-16 13:10:58 +0000

    - use getcwd()

--- src/testbed/test_getcwd.c
+++ src/testbed/test_getcwd.c
@@ -6,11 +6,7 @@ int main ()
   char buf[PATH_MAX];
   char *out;
   
-#ifdef MIGW
-  out = _getcwd (buf, PATH_MAX);
-#else
   out = getcwd (buf, PATH_MAX);
-#endif  /* MIGW */
   (void) printf ("CWD: %s\n", out);
   return 0;
 }
