c548217103fd9a463b3cc38774324edbb136d2c7
Christian Grothoff <christian@grothoff.org>
2013-10-18 12:01:22 +0000

    -better sleep pattern

--- src/gns/test_gns_rel_expiration.sh
+++ src/gns/test_gns_rel_expiration.sh
@@ -8,12 +8,13 @@ gnunet-identity -C testego -c test_gns_lookup.conf
 gnunet-identity -C delegatedego -c test_gns_lookup.conf
 DELEGATED_PKEY=$(gnunet-identity -d -c test_gns_lookup.conf | grep delegatedego | awk '{print $3}')
 gnunet-namestore -p -z testego -a -n b -t PKEY -V $DELEGATED_PKEY -e never -c test_gns_lookup.conf
-gnunet-namestore -p -z delegatedego -a -n www -t A -V $TEST_IP -e '2 s' -c test_gns_lookup.conf
+gnunet-namestore -p -z delegatedego -a -n www -t A -V $TEST_IP -e '5 s' -c test_gns_lookup.conf
 gnunet-arm -i gns -c test_gns_lookup.conf
-sleep 4
-gnunet-namestore -z delegatedego -d -n www -t A -V $TEST_IP  -e '2 s' -c test_gns_lookup.conf
+sleep 2
+gnunet-namestore -z delegatedego -d -n www -t A -V $TEST_IP  -e '5 s' -c test_gns_lookup.conf
+sleep 1
 RES_IP=`$DO_TIMEOUT gnunet-gns --raw -z testego -u www.b.gnu -t A -c test_gns_lookup.conf`
-sleep 4
+sleep 6
 RES_IP_EXP=`$DO_TIMEOUT gnunet-gns --raw -z testego -u www.b.gnu -t A -c test_gns_lookup.conf`
 gnunet-namestore -z testego -d -n b -t PKEY -V $DELEGATED_PKEY  -e never -c test_gns_lookup.conf
 gnunet-identity -D testego -c test_gns_lookup.conf
