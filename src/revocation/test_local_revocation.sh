97cd15acabbcfa2357aeecd15df3297a5cbe08aa
Matthias Wachs <wachs@net.in.tum.de>
2013-11-26 12:51:44 +0000

    evalutation

--- src/revocation/test_local_revocation.sh
+++ src/revocation/test_local_revocation.sh
@@ -9,19 +9,35 @@ trap "gnunet-arm -e -c test_revocation.conf" SIGINT
 rm -rf `gnunet-config -c test_revocation.conf -s PATHS -o GNUNET_HOME -f`
 
 # Start 
+RES=0
 gnunet-arm -s -c $TEST_CONFIGURATION
 gnunet-identity -C $TEST_REVOCATION_EGO -c $TEST_CONFIGURATION
 TEST_REVOCATION_KEY=$(gnunet-identity -d | awk '{split($0,a," "); print a[3]}')
 
 echo Testing key $TEST_REVOCATION_KEY
-OUTPUT_NOT_REVOKED=$(gnunet-revocation -t $TEST_REVOCATION_KEY -c $TEST_CONFIGURATION )
+OUTPUT_NOT_REVOKED=$(gnunet-revocation -t $TEST_REVOCATION_KEY -c $TEST_CONFIGURATION)
+if grep -q valid <<<$OUTPUT_NOT_REVOKED; 
+then
+		echo "Key was valid" 
+else
+    RES=1
+fi
 
 echo Revoking key $TEST_REVOCATION_KEY
 gnunet-revocation -R $TEST_REVOCATION_EGO -p -c $TEST_CONFIGURATION 1> /dev/null 2> /dev/null
 
 echo Testing revoked key $TEST_REVOCATION_KEY
 OUTPUT_REVOKED=$(gnunet-revocation -t $TEST_REVOCATION_KEY -c $TEST_CONFIGURATION)
+if grep -q revoked <<<$OUTPUT_REVOKED; 
+then
+    echo "Key was revoked"
+else
+    RES=1
+fi
+
 
 #clean up
-gnunet-arm -e -c test_revocation.conf
+gnunet-arm -e -c $TEST_CONFIGURATION
 rm -rf `gnunet-config -c test_revocation.conf -s PATHS -o GNUNET_HOME -f`
+
+exit $RES
