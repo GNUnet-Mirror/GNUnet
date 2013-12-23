f07469475bb254bae5b66a894087bad7d2d600bb
LRN <lrn1986@gmail.com>
2013-12-23 11:14:37 +0000

    Don't hardcode stat sizes

--- src/util/winproc.c
+++ src/util/winproc.c
@@ -147,8 +147,8 @@ GNInitWinEnv ()
   plibc_initialized ();
   plibc_set_panic_proc (plibc_panic);
   ret = plibc_init_utf8 ("GNU", PACKAGE, 1);
-  plibc_set_stat_size_size (8);
-  plibc_set_stat_time_size (4);
+  plibc_set_stat_size_size (sizeof (((struct stat *) 0)->st_size));
+  plibc_set_stat_time_size (sizeof (((struct stat *) 0)->st_mtime));
   /* don't load other DLLs twice */
   if (hNTDLL)
     return ret;
