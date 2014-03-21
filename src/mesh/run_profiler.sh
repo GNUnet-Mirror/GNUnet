273044e2abef1eeebcc07165f9148fd6ea7f4192
Bart Polot <bart@net.in.tum.de>
2014-03-21 18:44:34 +0000

    - wrap profiler in script to alter config file

---
+++ src/mesh/run_profiler.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$#" -lt "1" ]; then
+    echo "usage: $0 PEERS";
+    exit 1;
+fi
+
+PEERS=$1
+
+if [ $PEERS -eq 1 ]; then
+    echo "cannot run 1 peer";
+    exit 1;
+fi
+
+LINKS=`echo "l($PEERS) * $PEERS" | bc -l`
+LINKS=`printf "%.0f" $LINKS`
+echo "using $PEERS peers, $LINKS links";
+    
+sed -e "s/%LINKS%/$LINKS/g" profiler.template > profiler.conf
+
+./gnunet-mesh-profiler $PEERS
