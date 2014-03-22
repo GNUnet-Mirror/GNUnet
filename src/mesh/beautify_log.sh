28cecb117cc5755101afb0f7ec8de06210b1eea2
Bart Polot <bart@net.in.tum.de>
2014-03-22 02:58:19 +0000

    - optimize

--- src/mesh/beautify_log.sh
+++ src/mesh/beautify_log.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 cp log .log
 cat .log | grep "STARTING SERVICE " > __tmp_peers
-cat __tmp_peers | while read line; do
+SED_EXPR="a"
+while read -r line; do
     PEER=`echo $line | sed -e 's/.*\[\(....\)\].*/\1/'`
     PID=`echo $line | sed -e 's/.*mesh-\([0-9]*\).*/\1/'`
     echo "$PID => $PEER"
-    cat .log | sed -e "s/mesh-\([a-z2]*\)-$PID/MESH \1 $PEER/" > .log2
-    cat .log2 | sed -e "s/mesh-$PID/MESH XXX $PEER/" > .log3
-    mv .log3 .log
-done 
-
+    export SED_EXPR="${SED_EXPR}s/mesh-\([a-z2]*\)-$PID/MESH \1 $PEER/;"
+    export SED_EXPR="${SED_EXPR}s/mesh-$PID/MESH XXX $PEER/"
+done < __tmp_peers
 rm __tmp_peers
 
-cat .log | sed -e 's/mesh-api-/mesh-api-                                            /g' > .log2
+SED_EXPR="${SED_EXPR}s/mesh-api-/mesh-api-                                            /g"
+cat .log | sed -e "$SED_EXPR" > .log2
 mv .log2 .log
 
 if [[ "`ps aux | grep "kwrite .lo[g]"`" = "" ]]; then
