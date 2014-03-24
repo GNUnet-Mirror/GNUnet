ad10090b45f61b52ed90775cd912400503647675
Matthias Wachs <wachs@net.in.tum.de>
2014-03-24 10:22:07 +0000

    task was not marked as canceled when canceled fixing mantis #0003345

--- src/transport/gnunet-service-transport_manipulation.c
+++ src/transport/gnunet-service-transport_manipulation.c
@@ -696,6 +696,7 @@ GST_manipulation_peer_disconnect(const struct GNUNET_PeerIdentity *peer)
       if (GNUNET_SCHEDULER_NO_TASK != generic_send_delay_task)
         {
           GNUNET_SCHEDULER_cancel(generic_send_delay_task);
+          generic_send_delay_task = GNUNET_SCHEDULER_NO_TASK;
           if (NULL != generic_dqe_head)
             generic_send_delay_task = GNUNET_SCHEDULER_add_delayed(
                 GNUNET_TIME_absolute_get_remaining(generic_dqe_head->sent_at),
