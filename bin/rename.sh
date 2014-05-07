b3c9c0eae35e6419088b647791b85d0da93b1f4d
Bart Polot <bart@net.in.tum.de>
2014-05-07 12:07:47 +0000

    - rename also contents of config files, update config files

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for n in `find * -name "*.c"` `find * -name "*.h"` `find * -name "*.am"`
+for n in `find * -name "*.c"` `find * -name "*.h"` `find * -name "*.am"` `find * -name "*.conf"`
 do
  cat $n | sed -e "s/$1/$2/g" > $n.new
  mv $n.new $n || echo "Move failed: $n.new to $n"
