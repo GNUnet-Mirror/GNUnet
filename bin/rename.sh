3cfaa19a528dc191778e625dd2b3c28ed06cfdbe
Bart Polot <bart@net.in.tum.de>
2014-05-07 12:07:37 +0000

    - update .am files

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for n in `find * -name "*.c"` `find * -name "*.h"`
+for n in `find * -name "*.c"` `find * -name "*.h"` `find * -name "*.am"`
 do
  cat $n | sed -e "s/$1/$2/g" > $n.new
  mv $n.new $n || echo "Move failed: $n.new to $n"
