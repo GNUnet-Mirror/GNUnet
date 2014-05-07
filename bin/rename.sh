f845f5cdece28a41fad692e2839b398393d205e8
Bart Polot <bart@net.in.tum.de>
2014-05-07 13:00:40 +0000

    - rename in conf.in files, update conf.in file still referencing mesh

--- bin/rename.sh
+++ bin/rename.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for n in `find * -name "*.c"` `find * -name "*.h"` `find * -name "*.am"` `find * -name "*.conf"`
+for n in `find * -name "*.c"` `find * -name "*.h"` `find * -name "*.am"` `find * -name "*.conf"`  `find * -name "*.conf.in"` 
 do
  cat $n | sed -e "s/$1/$2/g" > $n.new
  mv $n.new $n || echo "Move failed: $n.new to $n"
