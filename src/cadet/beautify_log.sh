f941ea5b480477f936636a62f6df104e3e345698
Bart Polot <bart@net.in.tum.de>
2014-05-14 16:50:46 +0000

    - use cadet for log script

--- src/cadet/beautify_log.sh
+++ src/cadet/beautify_log.sh
@@ -3,14 +3,14 @@ grep "STARTING SERVICE " log > __tmp_peers
 SED_EXPR=""
 while read -r line; do
     PEER=`echo $line | sed -e 's/.*\[\(....\)\].*/\1/'`
-    PID=`echo $line | sed -e 's/.*mesh-\([0-9]*\).*/\1/'`
+    PID=`echo $line | sed -e 's/.*cadet-\([0-9]*\).*/\1/'`
     echo "$PID => $PEER"
-    SED_EXPR="${SED_EXPR}s/mesh-\([a-z2]*\)-$PID/MESH \1 $PEER/;"
-    SED_EXPR="${SED_EXPR}s/mesh-$PID/MESH XXX $PEER/;"
+    SED_EXPR="${SED_EXPR}s/cadet-\([a-z2]*\)-$PID/CADET \1 $PEER/;"
+    SED_EXPR="${SED_EXPR}s/cadet-$PID/CADET XXX $PEER/;"
 done < __tmp_peers
 rm __tmp_peers
 
-SED_EXPR="${SED_EXPR}s/mesh-api-/mesh-api-                                            /g"
+SED_EXPR="${SED_EXPR}s/cadet-api-/cadet-api-                                            /g"
 sed -e "$SED_EXPR" log > .log
 
 if [[ "`ps aux | grep "kwrite .lo[g]"`" = "" ]]; then
