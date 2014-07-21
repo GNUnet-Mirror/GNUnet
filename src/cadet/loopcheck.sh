a38d47ab43a73882e93a6090a3d30d183527354f
Bart Polot <bart@net.in.tum.de>
2014-07-21 00:15:04 +0000

    - allow to run specific checks in a loop

--- src/cadet/loopcheck.sh
+++ src/cadet/loopcheck.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-
-while true; do
-    date;
-    taskset 1 make check || break;
-    grep -B 10 Assert *log && break
-    ls core* &> /dev/null && break
-done
+if ["$1" == ""]; then
+    while true; do
+	date;
+	taskset 1 make check || break;
+	grep -B 10 Assert *log && break
+	ls core* &> /dev/null && break
+    done
+else
+    while true; do
+	date;
+	taskset 1 $1 |& tee log | grep -v DEBUG;
+	if [ "${PIPESTATUS[0]}" != "0" ]; then
+	    echo "Failed";
+	    date;
+	    break;
+	fi
+    done
+fi
