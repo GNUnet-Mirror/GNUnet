3554eab54a4b5fe1ee527bdda894bb3c2d59a3ba
Bart Polot <bart@net.in.tum.de>
2014-07-21 00:15:13 +0000

    - save old logfiles

--- src/cadet/loopcheck.sh
+++ src/cadet/loopcheck.sh
@@ -3,7 +3,8 @@ while true; do
     if [ "$1" == "" ]; then
 	taskset 1 make check || break;
     else
-	taskset 1 $1 |& tee log | grep -v DEBUG;
+	LOGFILE="`date "+%m.%d-%H:%M:%S"`.log"
+	taskset 1 $1 |& tee $LOGFILE | grep -v DEBUG;
 	if [ "${PIPESTATUS[0]}" != "0" ]; then
 	    echo "Failed";
 	    date;
