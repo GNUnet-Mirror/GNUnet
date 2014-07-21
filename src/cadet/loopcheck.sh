9ac0de85161e7cbe8795c7c275551276dd8666e4
Bart Polot <bart@net.in.tum.de>
2014-07-21 00:15:12 +0000

    - simplify loopcheck

--- src/cadet/loopcheck.sh
+++ src/cadet/loopcheck.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
-if ["$1" == ""]; then
-    while true; do
-	date;
+while true; do
+    if [ "$1" == "" ]; then
 	taskset 1 make check || break;
-	grep -B 10 Assert *log && break
-	ls core* &> /dev/null && break
-    done
-else
-    while true; do
-	date;
+    else
 	taskset 1 $1 |& tee log | grep -v DEBUG;
 	if [ "${PIPESTATUS[0]}" != "0" ]; then
 	    echo "Failed";
 	    date;
 	    break;
 	fi
-	grep -B 10 Assert log && break
-	ls core* &> /dev/null && break
-    done
-fi
+    fi
+    grep -B 10 Assert *log && break
+    ls core* &> /dev/null && break
+done
