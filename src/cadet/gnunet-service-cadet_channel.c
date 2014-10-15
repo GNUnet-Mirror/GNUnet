f00081f2abb7dd098f6c4433ca6117ce90c1adc7
Bart Polot <bart@net.in.tum.de>
2014-10-15 00:55:37 +0000

    - fix false assert trigger when message #0 is lost

--- src/cadet/gnunet-service-cadet_channel.c
+++ src/cadet/gnunet-service-cadet_channel.c
@@ -1536,7 +1536,7 @@ GCCH_send_data_ack (struct CadetChannel *ch, int fwd)
            GC_m2s (copy->type));
       continue;
     }
-    GNUNET_assert (copy->mid > ack);
+    GNUNET_assert (GC_is_pid_bigger(copy->mid, ack));
     delta = copy->mid - (ack + 1);
     if (63 < delta)
       break;
