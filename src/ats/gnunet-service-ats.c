f526573cfa977a227f45374fb9d89a8e0bfb3240
Christian Grothoff <christian@grothoff.org>
2015-02-08 23:17:43 +0000

    -fix shutdown order

--- src/ats/gnunet-service-ats.c
+++ src/ats/gnunet-service-ats.c
@@ -125,8 +125,8 @@ static void
 cleanup_task (void *cls,
               const struct GNUNET_SCHEDULER_TaskContext *tc)
 {
-  GAS_addresses_done ();
   GAS_connectivity_done ();
+  GAS_addresses_done ();
   GAS_plugin_done ();
   GAS_normalization_stop ();
   GAS_scheduling_done ();
