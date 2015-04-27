b1513688ccf724d0eaa97ff0001437979a77b475
Bart Polot <bart@net.in.tum.de>
2015-04-27 19:15:50 +0000

    - activate ratchet on receive

--- src/cadet/gnunet-service-cadet_tunnel.c
+++ src/cadet/gnunet-service-cadet_tunnel.c
@@ -1445,6 +1445,7 @@ t_ax_decrypt_and_validate (struct CadetTunnel *t, void *dst,
     ax->CKr = keys[2];
     ax->DHRr = *DHRp;
     ax->Nr = 0;
+    ax->ratchet_flag = GNUNET_YES;
   }
   else
   {
