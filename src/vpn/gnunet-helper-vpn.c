fd40063b16d12f18145a583ef10b925feb8d57df
Christian Grothoff <christian@grothoff.org>
2015-05-26 15:24:57 +0000

    -note

--- src/vpn/gnunet-helper-vpn.c
+++ src/vpn/gnunet-helper-vpn.c
@@ -360,6 +360,10 @@ run (int fd_tun)
   /* write refers to reading from stdin, writing to fd_tun */
   int write_open = 1;
 
+  /* FIXME: this should work with '&&', but then 2/5 VPN
+     tests fail. With '||' the tests pass, but this process
+     keeps running --- but only for the 'GNS' test ---
+     even though the stdout is closed :-(. Very confusing. */
   while ((1 == read_open) || (1 == write_open))
   {
     FD_ZERO (&fds_w);
