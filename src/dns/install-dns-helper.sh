8677cba802a0616f73fd16efe7ecd89752fb442e
Christian Grothoff <christian@grothoff.org>
2015-05-28 13:39:12 +0000

    also set permissions correctly if user 'gnunet' does not exist

--- src/dns/install-dns-helper.sh
+++ src/dns/install-dns-helper.sh
@@ -5,6 +5,8 @@
 $3 chown root $1/gnunet-helper-dns
 $3 chgrp $2 $1/gnunet-helper-dns
 $3 chmod 4750 $1/gnunet-helper-dns
+# In case user 'gnunet' does not exist, at least set the group
+$3 chgrp $2 $1/gnunet-service-dns
+# Usually we want both...
 $3 chown gnunet:$2 $1/gnunet-service-dns
 $3 chmod 2750 $1/gnunet-service-dns
-
