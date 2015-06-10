e73475e7931e307ab7110d546a5975e8eee03b21
Bart Polot <bart@net.in.tum.de>
2015-06-10 12:08:13 +0000

    - more messages to force errors

--- src/cadet/test_cadet.c
+++ src/cadet/test_cadet.c
@@ -33,7 +33,7 @@
 /**
  * How many messages to send
  */
-#define TOTAL_PACKETS 500
+#define TOTAL_PACKETS 5000 /* Cannot exceed 64k! */
 
 /**
  * How long until we give up on connecting the peers?
