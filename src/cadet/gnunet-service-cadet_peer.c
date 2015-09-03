e99a4e43e5eaac90ddd9c344f1bb5bfbc4a1be0e
Bart Polot <bart@net.in.tum.de>
2015-09-03 18:04:11 +0000

    - format

--- src/cadet/gnunet-service-cadet_peer.c
+++ src/cadet/gnunet-service-cadet_peer.c
@@ -2226,11 +2226,8 @@ GCP_remove_path (struct CadetPeer *peer, struct CadetPeerPath *path)
   GNUNET_assert (myid == path->peers[0]);
   GNUNET_assert (peer->id == path->peers[path->length - 1]);
 
-  LOG (GNUNET_ERROR_TYPE_INFO,
-       "Removing path %p (%u) from %s\n",
-       path,
-       path->length,
-       GCP_2s (peer));
+  LOG (GNUNET_ERROR_TYPE_INFO, "Removing path %p (%u) from %s\n",
+       path, path->length, GCP_2s (peer));
 
   for (iter = peer->path_head; NULL != iter; iter = next)
   {
