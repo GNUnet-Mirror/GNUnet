f8ab2ae102ea323c35185cbec55c7cd2803673ab
Bart Polot <bart@net.in.tum.de>
2015-10-02 15:07:43 +0000

    - only break for cadet errors

--- src/cadet/loopcheck.sh
+++ src/cadet/loopcheck.sh
@@ -3,7 +3,7 @@ while true; do
     if [ "$1" == "" ]; then
 	taskset 1 make check || break;
     else
-	LOGFILE="`date "+%m.%d-%H:%M:%S"`.log"
+	LOGFILE="test_`date "+%m.%d-%H:%M:%S"`.log"
 	taskset 1 $1 |& tee $LOGFILE | grep -v DEBUG;
 	if [ "${PIPESTATUS[0]}" != "0" ]; then
 	    echo "Failed";
@@ -11,7 +11,7 @@ while true; do
 	    break;
 	fi
     fi
-    grep -B 10 ERROR *?*.log && break
-    grep -B 10 Assert *?*.log && break
+    grep cadet test_*.log | grep -B 10 ERROR && break
+    grep cadet test_*.log | grep -B 10 Assert && break
     ls core* &> /dev/null && break
 done
