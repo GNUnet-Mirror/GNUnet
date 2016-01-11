1e53d26243bc986844a7b5b53bedeefb61dd7960
Gabor X Toth <*@tg-x.net>
2016-01-11 21:23:13 +0000

    doxygen config

---
+++ doc/doc.h
@@ -0,0 +1,11 @@
+/**
+ * @mainpage GNUnet documentation
+ *
+ * @section Introduction
+ *
+ * See the [Modules](modules.html) section for documentation of the various services, APIs and libraries of GNUnet.
+ *
+ * Some additional documentation can be found under [Files](files.html), in the src/include/ directory.
+ *
+ * See also the [handbooks](https://gnunet.org/handbooks) for installation, users, and developers.
+ */
