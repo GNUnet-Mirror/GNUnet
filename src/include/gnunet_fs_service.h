f95fe0a950bbb6e3c86ae6308c63466c77aed3e0
David Barksdale <amatus.amongus@gmail.com>
2016-01-22 20:35:06 +0000

    Fix typo

--- src/include/gnunet_fs_service.h
+++ src/include/gnunet_fs_service.h
@@ -2763,7 +2763,8 @@ struct GNUNET_FS_DirScanner;
  * Start a directory scanner.
  *
  * @param filename name of the directory to scan
- * @param disable_extractor #GNUNET_YES to not to run libextractor on files (only build a tree)
+ * @param disable_extractor #GNUNET_YES to not run libextractor on files (only
+ *        build a tree)
  * @param ex if not NULL, must be a list of extra plugins for extractor
  * @param cb the callback to call when there are scanning progress messages
  * @param cb_cls closure for @a cb
