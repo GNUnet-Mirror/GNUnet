ed1c91f77cc7402f3c012b7ff2fcb78443c6621f
Christian Grothoff <christian@grothoff.org>
2017-01-08 19:16:59 +0100

    remove unnecessary calls to gnunet-peerinfo that also caused the system to get stuck

--- src/hostlist/test_gnunet_daemon_hostlist_learning.c
+++ src/hostlist/test_gnunet_daemon_hostlist_learning.c
@@ -567,13 +567,6 @@ main (int argc, char *argv[])
   GNUNET_log_setup ("test-gnunet-daemon-hostlist",
                     "WARNING",
                     NULL);
-#if !WINDOWS
-  system ("gnunet-peerinfo -s -c test_learning_adv_peer.conf > /dev/null");
-  system ("gnunet-peerinfo -s -c test_learning_learn_peer.conf > /dev/null");
-#else
-  system ("gnunet-peerinfo -s -c test_learning_adv_peer.conf > NUL");
-  system ("gnunet-peerinfo -s -c test_learning_learn_peer.conf > NUL");
-#endif
   ret = check ();
   GNUNET_DISK_directory_remove ("/tmp/test-gnunet-hostlist-peer-1");
   GNUNET_DISK_directory_remove ("/tmp/test-gnunet-hostlist-peer-2");
