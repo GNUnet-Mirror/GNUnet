29f76b6fe373cd5ca7504be2ffdaad5500406b8b
Christian Grothoff <christian@grothoff.org>
2017-07-18 22:55:09 +0200

    simplify installation of nss plugins in build system, require user(s) to move plugins into place manually (#5078)

--- src/gns/nss/install-nss-plugin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# $1 - shell
-# $2 - top_builddir
-# $3 - nssdir
-# $4 - sudo binary (empty if root)
-$4 $1 $2/libtool --mode=finish $3
-echo LTINST: $4 $1 $2/libtool --mode=finish $3
-$4 rm -f $3/libnss_gns.la $3/libnss_gns4.la $3/libnss_gns6.la
