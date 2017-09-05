dca832adb5eb0ad336b0baaebf8d826c9b5ed125
ng0 <ng0@infotropique.org>
2017-09-05 12:08:13 +0000

    doc: gnunet-c-tutorial: include example 4 and 5.

---
+++ doc/tutorial-examples/004.c
@@ -0,0 +1,5 @@
+struct GNUNET_MessageHeader
+{
+  uint16_t size GNUNET_PACKED;
+  uint16_t type GNUNET_PACKED;
+};
---
+++ doc/tutorial-examples/005.c
@@ -0,0 +1,8 @@
+struct GNUNET_MQ_Envelope *env;
+struct GNUNET_MessageHeader *msg;
+
+env = GNUNET_MQ_msg_extra (msg, payload_size, GNUNET_MY_MESSAGE_TYPE);
+memcpy (&msg[1], &payload, payload_size);
+// Send message via message queue 'mq'
+GNUNET_mq_send (mq, env);
+
