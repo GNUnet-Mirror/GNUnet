7a0a978c78054fbf4d26a96aa091d576267b07cb
ng0 <ng0@infotropique.org>
2017-10-20 16:57:29 +0000

    fixes in installation.texi + some changes in gnunet-c-tutorial

--- doc/tutorial-examples/016.c
+++ doc/tutorial-examples/016.c
@@ -1,3 +1,4 @@
 void
-GNUNET_PEERSTORE_watch_cancel (struct GNUNET_PEERSTORE_WatchContext *wc);
+GNUNET_PEERSTORE_watch_cancel (struct GNUNET_PEERSTORE_WatchContext
+                               *wc);
 
