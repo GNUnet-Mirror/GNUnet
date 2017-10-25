94ee23b5e9c88d9965627d7deb692750df24869a
ng0 <ng0@infotropique.org>
2017-10-25 18:32:02 +0000

    fix

--- doc/documentation/run-gendocs.sh
+++ doc/documentation/run-gendocs.sh
@@ -2,5 +2,16 @@
 
 make version.texi
 make version2.texi
-./gendocs.sh --email gnunet-developers@gnu.org gnunet "GNUnet reference handbook"
-./gendocs.sh --email gnunet-developers@gnu.org gnunet-c-tutorial "GNUnet C Tutorial"
+./gendocs.sh --email gnunet-developers@gnu.org gnunet-c-tutorial "GNUnet C Tutorial" -o "manual/gnunet-c-tutorial"
+#cd manual
+#mkdir gnunet-c-tutorial
+#mv * gnunet-c-tutorial/
+#cd ..
+./gendocs.sh --email gnunet-developers@gnu.org gnunet "GNUnet reference handbook" -o "manual/gnunet"
+#cd manual
+#mkdir handbook
+#mkdir ../tmp-gnunet
+#mv gnunet ../tmp-gnunet
+#mv * handbook/
+#mv ../tmp-gnunet gnunet
+printf "Success"
