cbd60b5e56aac2d6711e299086383f83357794f8
psyc://loupsycedyglgamf.onion/~lynX <ircs://psyced.org/youbroketheinternet>
2018-06-05 23:12:01 +0000

    first batch of license fixes (boring)

--- src/ats-tests/gnunet-ats-sim.c
+++ src/ats-tests/gnunet-ats-sim.c
@@ -2,20 +2,15 @@
  This file is part of GNUnet.
  Copyright (C) 2010-2013 GNUnet e.V.
 
- GNUnet is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published
- by the Free Software Foundation; either version 3, or (at your
- option) any later version.
+ GNUnet is free software: you can redistribute it and/or modify it
+ under the terms of the GNU General Public License as published
+ by the Free Software Foundation, either version 3 of the License,
+ or (at your option) any later version.
 
  GNUnet is distributed in the hope that it will be useful, but
  WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with GNUnet; see the file COPYING.  If not, write to the
- Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- Boston, MA 02110-1301, USA.
+ Affero General Public License for more details.
  */
 /**
  * @file ats-tests/gnunet-ats-sim.c
