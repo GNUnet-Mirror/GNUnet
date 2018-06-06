be9319fee506932c3d7f731f62884e8bd96431c5
psyc://loupsycedyglgamf.onion/~lynX <ircs://psyced.org/youbroketheinternet>
2018-06-06 06:20:54 +0000

    second batch complete. WE ARE AFFERO AGPL NOW!

--- src/sq/test_sq.c
+++ src/sq/test_sq.c
@@ -2,16 +2,15 @@
   This file is part of GNUnet
   (C) 2015, 2016, 2017 GNUnet e.V.
 
-  GNUnet is free software; you can redistribute it and/or modify it under the
-  terms of the GNU General Public License as published by the Free Software
-  Foundation; either version 3, or (at your option) any later version.
+  GNUnet is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published
+  by the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
 
-  GNUnet is distributed in the hope that it will be useful, but WITHOUT ANY
-  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License along with
-  GNUnet; see the file COPYING.  If not, If not, see <http://www.gnu.org/licenses/>
+  GNUnet is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Affero General Public License for more details.
 */
 /**
  * @file sq/test_sq.c
