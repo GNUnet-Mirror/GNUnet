4e2504a967ba09643c6dd7e3b9ce400e30adcb3d
psyc://loupsycedyglgamf.onion/~lynX <ircs://psyced.org/youbroketheinternet>
2018-06-07 15:23:35 +0000

    paragraph for gnunet devs that don't know how to use the web

--- src/fs/test_fs_uri.c
+++ src/fs/test_fs_uri.c
@@ -11,6 +11,9 @@
      WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
      Affero General Public License for more details.
+    
+     You should have received a copy of the GNU Affero General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
  * @file fs/test_fs_uri.c
--- src/transport/gnunet-helper-transport-bluetooth.c
+++ src/transport/gnunet-helper-transport-bluetooth.c
@@ -13,6 +13,9 @@
    WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Affero General Public License for more details.
+  
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "gnunet_config.h"
 
