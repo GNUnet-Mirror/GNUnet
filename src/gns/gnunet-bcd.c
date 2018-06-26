61912531ba7971fb42afc4615d18122e1c9df913
xrs <xrs@mail36.net>
2018-06-26 13:21:51 +0200

    gnunet-bcd.c: minor fix

--- src/gns/gnunet-bcd.c
+++ src/gns/gnunet-bcd.c
@@ -469,7 +469,7 @@ run (void *cls,
                               "open",
                               fn);
     GNUNET_free (fn);
-    CLOSE (fd);
+    GNUNET_break (0 == CLOSE (fd));
     return;
   }
   GNUNET_free (fn);
@@ -499,7 +499,7 @@ run (void *cls,
     return;
   GNUNET_SCHEDULER_add_shutdown (&server_stop,
 				 NULL);
-  CLOSE(fd);
+  GNUNET_break (0 == CLOSE(fd));
 }
 
 
