4af6e380bbda66c267737c753ee8357c8b99b0fa
t3sserakt <t3ss@posteo.de>
2018-06-26 14:30:18 +0200

    codesonar finding 1575.9537

--- src/cadet/gnunet-service-cadet_peer.c
+++ src/cadet/gnunet-service-cadet_peer.c
@@ -243,7 +243,11 @@ GCP_2s (const struct CadetPeer *cp)
   static char buf[5];
   char *ret;
 
+  if (NULL == cp || NULL == &cp->pid.public_key)
+      return "NULL";
   ret = GNUNET_CRYPTO_eddsa_public_key_to_string (&cp->pid.public_key);
+  if (NULL == ret)
+      return "NULL";
   strncpy (buf,
            ret,
            sizeof (buf) - 1);
