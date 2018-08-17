cfb3c27290ab28f4b081908536fca1910ae364fb
Schanzenbach, Martin <martin.schanzenbach@aisec.fraunhofer.de>
2018-08-17 15:05:38 +0200

    copyright

--- src/zklaim/zklaim_functions.c
+++ src/zklaim/zklaim_functions.c
@@ -1,3 +1,28 @@
+/*
+   This file is part of GNUnet.
+   Copyright (C) 2012-2015 GNUnet e.V.
+
+   GNUnet is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Affero General Public License as published
+   by the Free Software Foundation, either version 3 of the License,
+   or (at your option) any later version.
+
+   GNUnet is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Affero General Public License for more details.
+  
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   */
+/**
+ * @author Martin Schanzenbach
+ * @file src/zklaim/zklaim_functions.c
+ * @brief zklaim functions
+ *
+ */
+
+
 #include "platform.h"
 #include "zklaim/zklaim.h"
 #include "gcrypt.h"
