d8505f5da65fc8269505621ff3284df2aa980edd
Julius Bünger <buenger@mytum.de>
2018-11-22 16:29:47 +0100

    RPS tests: Get rid of warning (unused argument)

--- src/rps/test_service_rps_sampler_elem.c
+++ src/rps/test_service_rps_sampler_elem.c
@@ -11,7 +11,7 @@
      WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
      Affero General Public License for more details.
-    
+
      You should have received a copy of the GNU Affero General Public License
      along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -194,6 +194,9 @@ check ()
 int
 main (int argc, char *argv[])
 {
+  (void) argc;
+  (void) argv;
+
   GNUNET_log_setup ("test_service_rps_peers", 
 		    "WARNING",
 		    NULL);
