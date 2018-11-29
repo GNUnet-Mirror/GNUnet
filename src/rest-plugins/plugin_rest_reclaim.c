a67b4de710264a1b18ee80f153dc5d9a806e4a85
Schanzenbach, Martin <mschanzenbach@posteo.de>
2018-11-29 19:15:11 +0100

    JSON: Remove jsonapi headers

--- src/rest-plugins/plugin_rest_reclaim.c
+++ src/rest-plugins/plugin_rest_reclaim.c
@@ -30,8 +30,6 @@
 #include "gnunet_gnsrecord_lib.h"
 #include "gnunet_namestore_service.h"
 #include "gnunet_rest_lib.h"
-#include "gnunet_jsonapi_lib.h"
-#include "gnunet_jsonapi_util.h"
 #include "microhttpd.h"
 #include <jansson.h>
 #include <inttypes.h>
