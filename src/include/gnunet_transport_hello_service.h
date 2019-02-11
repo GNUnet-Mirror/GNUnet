270b5b2ad07eb6c419078e4b7ceea9791c5625f6
Christian Grothoff <christian@grothoff.org>
2019-02-11 21:38:36 +0100

    add API for address injection

--- src/include/gnunet_transport_hello_service.h
+++ src/include/gnunet_transport_hello_service.h
@@ -11,7 +11,7 @@
      WITHOUT ANY WARRANTY; without even the implied warranty of
      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
      Affero General Public License for more details.
-    
+
      You should have received a copy of the GNU Affero General Public License
      along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -107,7 +107,7 @@ enum GNUNET_TRANSPORT_AddressClass
    * Bitmask for "any" address.
    */
   GNUNET_TRANSPORT_AC_ANY = 65535
-  
+
 };
 
 
