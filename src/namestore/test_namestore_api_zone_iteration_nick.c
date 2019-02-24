c1f87d457fa51d9d85f5b35f808b802fa14088bb
Christian Grothoff <christian@grothoff.org>
2019-02-24 12:43:46 +0100

    avoid failing hard if 'gnunetcheck' db does not exist  (or plugins cannot be loaded for other reasons)

--- src/namestore/test_namestore_api_zone_iteration_nick.c
+++ src/namestore/test_namestore_api_zone_iteration_nick.c
@@ -480,19 +480,17 @@ run (void *cls,
 }
 
 
+#include "test_common.c"
+
+
 int
 main (int argc, char *argv[])
 {
   const char *plugin_name;
   char *cfg_name;
 
-  plugin_name = GNUNET_TESTING_get_testname_from_underscore (argv[0]);
-  GNUNET_asprintf (&cfg_name,
-                   "test_namestore_api_%s.conf",
-                   plugin_name);
+  SETUP_CFG (plugin_name, cfg_name);
   res = 1;
-  GNUNET_DISK_purge_cfg_dir (cfg_name,
-                             "GNUNET_TEST_HOME");
   if (0 !=
       GNUNET_TESTING_peer_run ("test-namestore-api-zone-iteration-nick",
                                cfg_name,
