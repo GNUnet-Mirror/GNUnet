25af6d913578ed7176d22d0eb187ad2a70d49374
ng0 <ng0@n0.is>
2019-03-04 14:37:50 +0000

    safety checks for lint-man.sh

--- contrib/scripts/lint-man.sh
+++ contrib/scripts/lint-man.sh
@@ -3,16 +3,29 @@
 # SPDX-License-Identifier: 0BSD
 # spit out ONLY error messages using groff.
 
-echo "groff check"
-for f in `find . -name \*\.[1-9]`;
-do
-    LC_ALL=en_US.UTF-8 \
-    MANROFFSEQ='' \
-    MANWIDTH=80 \
-    groff -m mandoc -b -z -w w $f;
-done
+existence()
+{
+    command -v "$1" >/dev/null 2>&1
+}
+
+if existence groff;
+then
+    echo "groff check"
+    for f in `find . -name \*\.[1-9]`;
+    do
+        LC_ALL=en_US.UTF-8 \
+              MANROFFSEQ='' \
+              MANWIDTH=80 \
+              groff -m mandoc -b -z -w w $f;
+    done
+fi
+
 echo "mandoc check"
 # spit out ONLY error messages with mandoc:
-mandoc -T lint `find . -name \*\.[1-9]`
+if existence mandoc;
+then
+    mandoc -T lint `find . -name \*\.[1-9]`
+fi
+
 #LC_ALL=en_US.UTF-8 MANROFFSEQ='' MANWIDTH=80 /run/current-system/profile/bin/man --warnings -E UTF-8 -l -Tutf8 -Z <*.5> >report5.log
 #LC_ALL=en_US.UTF-8 MANROFFSEQ='' MANWIDTH=80 /run/current-system/profile/bin/man --warnings -E UTF-8 -l -Tutf8 -Z <*.1> >report1.log
