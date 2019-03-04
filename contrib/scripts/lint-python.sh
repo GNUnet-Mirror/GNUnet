d4cea440095e8447e89daef039fdf8f251f2663d
ng0 <ng0@n0.is>
2019-03-04 13:49:36 +0000

    Make contrib/scripts/lint-python.sh useful

--- contrib/scripts/lint-python.sh
+++ contrib/scripts/lint-python.sh
@@ -1,24 +1,76 @@
 #!/bin/sh
 # check python style (and 2 to 3 migration)
+#
+# behold, the worst lowest effort shell script
+# ...given that we have more generic checking
+# for executables in other scripts already
 
-rm python-lint.log
+existence()
+{
+    command -v "$1" >/dev/null 2>&1
+}
 
-if [ -e "python" ]
+# invoke from root of source!
+if [ $(basename $(pwd)) = "scripts" ]
 then
-    python --version >> python-lint.log
+   return 1
+else
+    if [ -e "python-lint.log" ]
+    then
+        rm "python-lint.log"
+    fi
+
+    if existence python;
+    then
+        python --version >> python-lint.log
+    fi
+
+    if existence python2;
+    then
+        python2 --version >> python-lint.log
+    fi
+
+    if existence python3;
+    then
+        python3 --version >> python-lint.log
+    fi
+
+    if existence python3.7;
+    then
+        python3.7 --version >> python-lint.log
+    fi
+
+    if existence flake8;
+    then
+        echo >> python-lint.log
+        echo "flake8:" >> python-lint.log
+        echo >> python-lint.log
+        flake8 >> python-lint.log
+    fi
+
+    if existence flake8-3.7;
+    then
+        echo >> python-lint.log
+        echo "flake8:" >> python-lint.log
+        echo >> python-lint.log
+        flake8-3.7 >> python-lint.log
+    fi
+
+    if existence 2to3;
+    then
+        echo >> python-lint.log
+        echo "2to3" >> python-lint.log
+        echo >> python-lint.log
+        2to3 -v -d . >> python-lint.log
+        2to3 -v -p . >> python-lint.log
+    fi
+
+    if existence 2to3-3.7;
+    then
+        echo >> python-lint.log
+        echo "2to3" >> python-lint.log
+        echo >> python-lint.log
+        2to3-3.7 -v -d . >> python-lint.log
+        2to3-3.7 -v -p . >> python-lint.log
+    fi
 fi
-
-if [ -e "python2" ]
-then
-    python2 --version >> python-lint.log
-fi
-
-if [ -e "python3" ]
-then
-    python3 --version >> python-lint.log
-fi
-
-flake8 >> python-lint.log
-
-2to3 -v -d . >> python-lint.log
-2to3 -v -p . >> python-lint.log
