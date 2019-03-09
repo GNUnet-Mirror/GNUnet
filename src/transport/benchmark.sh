9eaffe89fa9b664faaf5a82351f81167c240fc3b
ng0 <ng0@n0.is>
2019-03-09 11:47:53 +0000

    bashism

--- src/transport/benchmark.sh
+++ src/transport/benchmark.sh
@@ -1,15 +1,13 @@
-#/bin/sh
+#!/bin/sh
 
-for i in {1..0}
+for i in $(seq 1 0)
 do
  echo RUN $i
  ./test_transport_api_reliability_http
 done
 
-for i in {1..100}
+for i in $(seq 1 100)
 do
  echo RUN $i
  ./test_transport_api_reliability_https
 done
- 
- 
