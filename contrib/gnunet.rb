94eb76c41311d00f2a44c103e0c4536343b3a4c8
Schanzenbach, Martin <mschanzenbach@posteo.de>
2019-06-06 10:26:10 +0200

    update homebrew formula to v0.11.5

--- contrib/gnunet.rb
+++ contrib/gnunet.rb
@@ -1,14 +1,14 @@
 class Gnunet < Formula
   desc "Framework for distributed, secure and privacy-preserving applications"
   homepage "https://gnunet.org/"
-  url "https://ftp.gnu.org/gnu/gnunet/gnunet-0.11.3.tar.gz"
-  sha256 "2405db9232ae6ded57e7ff513abdf810c65e3861823b3985717ce990b8d87a37"
+  url "https://ftp.gnu.org/gnu/gnunet/gnunet-0.11.5.tar.gz"
+  sha256 "98e0355ff0627bf88112b3b92a7522e98c0ae6071fc45efda5a33daed28199b3"
 
   bottle do
     cellar :any
-    sha256 "8a28d2c64bf814bcd629b66715b553bee3031c05f98075dd0f1bc79acb4fe840" => :mojave
-    sha256 "9fc7995800f8c74266313e9ccc274b625debc4033221dae729936821400a4c87" => :high_sierra
-    sha256 "c75feb5d2bfcb379c0025645d0d8ab7c2e5fce938901ec53c841bbeafbf684c1" => :sierra
+    sha256 "3c2971584ed0a709b5c59c3f844e5966049b90461043af93ba10e167c134a284" => :mojave
+    sha256 "81e3f400e41674f919a2656217bd4e1ce825505d9e3939acdc0b3efcfa8949d4" => :high_sierra
+    sha256 "0d5a57bacf57f3a78b2a96e1c0b9d22db5385255211b5bd9b8334ffe57925136" => :sierra
   end
 
   depends_on "pkg-config" => :build
