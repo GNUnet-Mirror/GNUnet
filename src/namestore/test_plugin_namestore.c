98f2f7c900d0b31a0b05b06e77993a9ee7966d32
Christian Grothoff <christian@grothoff.org>
2019-07-24 13:48:06 +0200

    add NZL

--- src/namestore/test_plugin_namestore.c
+++ src/namestore/test_plugin_namestore.c
@@ -49,12 +49,8 @@ unload_plugin (struct GNUNET_NAMESTORE_PluginFunctions *api)
 {
   char *libname;
 
-  GNUNET_asprintf (&libname,
-                   "libgnunet_plugin_namestore_%s",
-                   plugin_name);
-  GNUNET_break (NULL ==
-                GNUNET_PLUGIN_unload (libname,
-                                      api));
+  GNUNET_asprintf (&libname, "libgnunet_plugin_namestore_%s", plugin_name);
+  GNUNET_break (NULL == GNUNET_PLUGIN_unload (libname, api));
   GNUNET_free (libname);
 }
 
@@ -72,16 +68,12 @@ load_plugin (const struct GNUNET_CONFIGURATION_Handle *cfg)
   char *libname;
 
   GNUNET_log (GNUNET_ERROR_TYPE_INFO,
-              _("Loading `%s' namestore plugin\n"),
+              _ ("Loading `%s' namestore plugin\n"),
               plugin_name);
-  GNUNET_asprintf (&libname,
-                   "libgnunet_plugin_namestore_%s",
-                   plugin_name);
-  if (NULL == (ret = GNUNET_PLUGIN_load (libname, (void*) cfg)))
+  GNUNET_asprintf (&libname, "libgnunet_plugin_namestore_%s", plugin_name);
+  if (NULL == (ret = GNUNET_PLUGIN_load (libname, (void *) cfg)))
   {
-    FPRINTF (stderr,
-             "Failed to load plugin `%s'!\n",
-             plugin_name);
+    FPRINTF (stderr, "Failed to load plugin `%s'!\n", plugin_name);
     GNUNET_free (libname);
     return NULL;
   }
@@ -92,7 +84,7 @@ load_plugin (const struct GNUNET_CONFIGURATION_Handle *cfg)
 
 static void
 test_record (void *cls,
-	     uint64_t seq,
+             uint64_t seq,
              const struct GNUNET_CRYPTO_EcdsaPrivateKey *private_key,
              const char *label,
              unsigned int rd_count,
@@ -104,69 +96,54 @@ test_record (void *cls,
   char tname[64];
   unsigned int trd_count = 1 + (id % 1024);
 
-  GNUNET_snprintf (tname,
-                   sizeof (tname),
-		   "a%u",
-                   (unsigned int) id);
+  GNUNET_snprintf (tname, sizeof (tname), "a%u", (unsigned int) id);
   GNUNET_assert (trd_count == rd_count);
-  for (unsigned int i=0;i<trd_count;i++)
+  for (unsigned int i = 0; i < trd_count; i++)
   {
     GNUNET_assert (rd[i].data_size == id % 10);
     GNUNET_assert (0 == memcmp ("Hello World", rd[i].data, id % 10));
     GNUNET_assert (rd[i].record_type == TEST_RECORD_TYPE);
     GNUNET_assert (rd[i].flags == 0);
   }
-  memset (&tzone_private_key,
-	  (id % 241),
-	  sizeof (tzone_private_key));
+  memset (&tzone_private_key, (id % 241), sizeof (tzone_private_key));
   GNUNET_assert (0 == strcmp (label, tname));
-  GNUNET_assert (0 == GNUNET_memcmp (&tzone_private_key,
-                              private_key));
+  GNUNET_assert (0 == GNUNET_memcmp (&tzone_private_key, private_key));
 }
 
 
 static void
-get_record (struct GNUNET_NAMESTORE_PluginFunctions *nsp,
-            int id)
+get_record (struct GNUNET_NAMESTORE_PluginFunctions *nsp, int id)
 {
-  GNUNET_assert (GNUNET_OK ==
-                 nsp->iterate_records (nsp->cls,
-                                       NULL,
-                                       0,
-                                       1,
-                                       &test_record,
-                                       &id));
+  GNUNET_assert (
+    GNUNET_OK ==
+    nsp->iterate_records (nsp->cls, NULL, 0, 1, &test_record, &id));
 }
 
 
 static void
-put_record (struct GNUNET_NAMESTORE_PluginFunctions *nsp,
-            int id)
+put_record (struct GNUNET_NAMESTORE_PluginFunctions *nsp, int id)
 {
   struct GNUNET_CRYPTO_EcdsaPrivateKey zone_private_key;
   char label[64];
   unsigned int rd_count = 1 + (id % 1024);
-  struct GNUNET_GNSRECORD_Data rd[rd_count];
+  struct GNUNET_GNSRECORD_Data rd[GNUNET_NZL (rd_count)];
   struct GNUNET_CRYPTO_EcdsaSignature signature;
 
-  GNUNET_snprintf (label, sizeof (label),
-		   "a%u", (unsigned int ) id);
-  for (unsigned int i=0;i<rd_count;i++)
+  GNUNET_snprintf (label, sizeof (label), "a%u", (unsigned int) id);
+  for (unsigned int i = 0; i < rd_count; i++)
   {
     rd[i].data = "Hello World";
     rd[i].data_size = id % 10;
-    rd[i].expiration_time = GNUNET_TIME_relative_to_absolute (GNUNET_TIME_UNIT_MINUTES).abs_value_us;
+    rd[i].expiration_time =
+      GNUNET_TIME_relative_to_absolute (GNUNET_TIME_UNIT_MINUTES).abs_value_us;
     rd[i].record_type = TEST_RECORD_TYPE;
     rd[i].flags = 0;
   }
   memset (&zone_private_key, (id % 241), sizeof (zone_private_key));
   memset (&signature, (id % 243), sizeof (signature));
-  GNUNET_assert (GNUNET_OK ==
-		 nsp->store_records (nsp->cls,
-				     &zone_private_key,
-				     label,
-				     rd_count,
-				     rd));
+  GNUNET_assert (
+    GNUNET_OK ==
+    nsp->store_records (nsp->cls, &zone_private_key, label, rd_count, rd));
 }
 
 
@@ -182,9 +159,10 @@ run (void *cls,
   nsp = load_plugin (cfg);
   if (NULL == nsp)
   {
-    FPRINTF (stderr,
-             "%s",
-	     "Failed to initialize namestore.  Database likely not setup, skipping test.\n");
+    FPRINTF (
+      stderr,
+      "%s",
+      "Failed to initialize namestore.  Database likely not setup, skipping test.\n");
     return;
   }
   put_record (nsp, 1);
@@ -195,30 +173,19 @@ run (void *cls,
 
 
 int
-main (int argc,
-      char *argv[])
+main (int argc, char *argv[])
 {
   char cfg_name[PATH_MAX];
-  char *const xargv[] = {
-    "test-plugin-namestore",
-    "-c",
-    cfg_name,
-    NULL
-  };
-  struct GNUNET_GETOPT_CommandLineOption options[] = {
-    GNUNET_GETOPT_OPTION_END
-  };
+  char *const xargv[] = {"test-plugin-namestore", "-c", cfg_name, NULL};
+  struct GNUNET_GETOPT_CommandLineOption options[] = {GNUNET_GETOPT_OPTION_END};
 
-  GNUNET_log_setup ("test-plugin-namestore",
-                    "WARNING",
-                    NULL);
+  GNUNET_log_setup ("test-plugin-namestore", "WARNING", NULL);
   plugin_name = GNUNET_TESTING_get_testname_from_underscore (argv[0]);
   GNUNET_snprintf (cfg_name,
                    sizeof (cfg_name),
                    "test_plugin_namestore_%s.conf",
                    plugin_name);
-  GNUNET_DISK_purge_cfg_dir (cfg_name,
-                             "GNUNET_TMP");
+  GNUNET_DISK_purge_cfg_dir (cfg_name, "GNUNET_TMP");
   GNUNET_PROGRAM_run ((sizeof (xargv) / sizeof (char *)) - 1,
                       xargv,
                       "test-plugin-namestore",
@@ -226,12 +193,9 @@ main (int argc,
                       options,
                       &run,
                       NULL);
-  GNUNET_DISK_purge_cfg_dir (cfg_name,
-                             "GNUNET_TMP");
+  GNUNET_DISK_purge_cfg_dir (cfg_name, "GNUNET_TMP");
   if (ok != 0)
-    FPRINTF (stderr,
-             "Missed some testcases: %d\n",
-             ok);
+    FPRINTF (stderr, "Missed some testcases: %d\n", ok);
   return ok;
 }
 
