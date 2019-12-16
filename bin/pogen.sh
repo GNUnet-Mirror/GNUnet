73ee949868200082b7e9131d2f410f289fd1c416
Christian Grothoff <christian@grothoff.org>
2019-12-16 17:17:46 +0100

    sort using C locale for a more stable sort

--- bin/pogen.sh
+++ bin/pogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # This script is in the public domain.
+
+# Set language domain to C for a more stable sort!
+export LANG=C
+
 find src -name "*.c" | grep -v \# | grep -v /test_ | grep -v /perf_  | grep -v _old | grep -v chat | grep -v .libs/ | sort  > po/POTFILES.in
 grep -l _\( `find src -name "*.h"` | grep -v "platform.h" | grep -v _old | grep -v chat | sort >> po/POTFILES.in
 
